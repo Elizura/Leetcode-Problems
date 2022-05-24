bc30355e3cfc0d33d99abbdda512e3a215592466
Elizura <98797447+Elizura@users.noreply.github.com>
2022-05-24 14:14:30 -0700

    Time: 38 ms (59.96%), Space: 13.9 MB (11.23%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.py
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.py
@@ -1,18 +1,18 @@
 class Solution:
     def sortSentence(self, s: str) -> str:
         a=[]
-        sl=s.split(' ')
-        b=[0]*len(sl)
-        c=''
+        list_of_the_sentence =s.split(' ')
+        b=[0]*len(list_of_the_sentence)
+        Final_String=''
 
         for i in s:
             if i in '123456789':
                 a.append(int(i)-1)
-        for i in range(len(sl)):
-            b[a[i]]=sl[i]
+        for i in range(len(list_of_the_sentence)):
+            b[a[i]]=list_of_the_sentence[i]
 
         e= ' '.join(map(str,b))
         for i in e:
             if i not in '123456789':
-                c+=i
-        return c
+                Final_String+=i
+        return Final_String
