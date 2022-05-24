d88a71942c8469575d5e28c9e4c11f279121d66d
Elizura <98797447+Elizura@users.noreply.github.com>
2022-05-24 06:58:07 -0700

    Create ValidPar.py

---
+++ ValidPar.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        b=[]
+        a={')':'(',
+           '}':'{',
+           ']':'['}
+        for i in s:
+            if i in a.values():
+                b.append(i)
+            if i in a.keys():
+                if b and b[-1]==a[i]:
+                    b.pop()
+                else:
+                    return False
+        return False if b else True
