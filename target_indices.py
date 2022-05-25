4a198f2838dda3ffeab0e15a14166f89ae6b7920
Elizura <98797447+Elizura@users.noreply.github.com>
2022-05-25 01:20:55 -0700

    Create target_indices.py

---
+++ target_indices.py
@@ -0,0 +1,7 @@
+    def targetIndices(self, nums: List[int], target: int) -> List[int]:
+        nums.sort()
+        output=[]
+        for i in range(len(nums)):
+            if nums[i]== target:
+                output.append(i)
+        return output
