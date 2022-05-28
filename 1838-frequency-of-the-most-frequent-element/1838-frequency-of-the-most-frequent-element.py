11456fb5dfb0ba0a759109bcda90f38065ee4a51
Elizura <98797447+Elizura@users.noreply.github.com>
2022-05-28 06:41:15 -0700

    Time: 3059 ms (12.65%), Space: 28 MB (27.82%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.py
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.py
@@ -2,8 +2,8 @@ class Solution:
     def maxFrequency(self, nums: List[int], k: int) -> int:
         nums.sort()
         largest_freq=0
-        l=r=0
-        total=0
+        l=r=0 #right and left pointers set to 0
+        total=0 #basically used for the formula but nothing else
         for i in range(len(nums)):
             total+=nums[r]
             
@@ -12,7 +12,7 @@ class Solution:
                 #shrink the size by moving the left pointer forwards
                 total-=nums[l]
                 l+=1
-            largest_freq=max(largest_freq,r-l+1)
+            largest_freq=max(largest_freq,r-l+1) #updating the largest frequency
             r+=1
         return largest_freq
                 
