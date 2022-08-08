1af10b029a8db09fad41bf06dd9692b67da543c1
Elizura <98797447+Elizura@users.noreply.github.com>
2022-08-08 15:44:51 -0700

    Time: 614 ms (84.73%), Space: 35.9 MB (68.62%) - LeetHub

--- 295-find-median-from-data-stream/295-find-median-from-data-stream.py
+++ 295-find-median-from-data-stream/295-find-median-from-data-stream.py
@@ -6,15 +6,18 @@ class MedianFinder:
         
         
     def addNum(self, num: int) -> None:
-        heapq.heappush(self.minHeap, num)
-        heapq.heappush(self.maxHeap, -heapq.heappop(self.minHeap))
+        heapq.heappush(self.maxHeap, -heapq.heappushpop(self.minHeap, num))        
+        # heapq.heappush(self.minHeap, num)
+        # heapq.heappush(self.maxHeap, -heapq.heappop(self.minHeap))
         if len(self.maxHeap) > len(self.minHeap):
             heapq.heappush(self.minHeap, -heapq.heappop(self.maxHeap))
+            
     def findMedian(self) -> float:
-        if len(self.minHeap) == len(self.maxHeap):
-            return (self.minHeap[0] + (-self.maxHeap[0]))/2
-        else:
-            return self.minHeap[0]
+        return 0.5*(self.minHeap[0] + (-self.maxHeap[0])) if len(self.minHeap) == len(self.maxHeap) else self.minHeap[0]
+#         if len(self.minHeap) == len(self.maxHeap):
+#             return (self.minHeap[0] + (-self.maxHeap[0]))/2
+#         else:
+#             return self.minHeap[0]
         
 
 
