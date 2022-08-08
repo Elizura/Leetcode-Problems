a3572d2b386b062dd24c3e0f3ad3047044766d10
Elizura <98797447+Elizura@users.noreply.github.com>
2022-08-08 15:30:20 -0700

    Time: 623 ms (83.34%), Space: 36.5 MB (12.47%) - LeetHub

--- 295-find-median-from-data-stream/295-find-median-from-data-stream.py
+++ 295-find-median-from-data-stream/295-find-median-from-data-stream.py
@@ -6,43 +6,15 @@ class MedianFinder:
         
         
     def addNum(self, num: int) -> None:
-        # print(self.maxHeap, self.minHeap)
-        if not self.minHeap and not self.maxHeap:             
-            heapq.heappush(self.minHeap, num)
-        elif self.minHeap and not self.maxHeap:
-            if num > self.minHeap[0]:
-                heapq.heappush(self.minHeap, num)
-                a = heapq.heappop(self.minHeap)
-                heapq.heappush(self.maxHeap, -a)                    
-            else:
-                heapq.heappush(self.maxHeap, -num)         
-        elif len(self.minHeap) == len(self.maxHeap) + 1:
-            if num > self.minHeap[0]:
-                heapq.heappush(self.minHeap, num)
-                a = heapq.heappop(self.minHeap)
-                heapq.heappush(self.maxHeap, -a)                
-            else:
-                heapq.heappush(self.maxHeap, -num)           
-        elif len(self.minHeap) == len(self.maxHeap):            
-            if num > self.minHeap[0]:
-                heapq.heappush(self.minHeap, num)
-            else:
-                heapq.heappush(self.maxHeap, -num)   
-        else:
-            if num > self.minHeap[0]:
-                heapq.heappush(self.minHeap, num)                
-            else:
-                heapq.heappush(self.maxHeap, -num)
-                a = heapq.heappop(self.maxHeap) 
-                heapq.heappush(self.minHeap, -a)
-                
+        heapq.heappush(self.minHeap, num)
+        heapq.heappush(self.maxHeap, -heapq.heappop(self.minHeap))
+        if len(self.maxHeap) > len(self.minHeap):
+            heapq.heappush(self.minHeap, -heapq.heappop(self.maxHeap))
     def findMedian(self) -> float:
         if len(self.minHeap) == len(self.maxHeap):
             return (self.minHeap[0] + (-self.maxHeap[0]))/2
-        elif len(self.minHeap) > len(self.maxHeap):
-            return self.minHeap[0]
         else:
-            return -self.maxHeap[0]
+            return self.minHeap[0]
         
 
 
