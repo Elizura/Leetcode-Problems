cf59d44973f9fa8b0e63c184382e0d3f0ce69f61
Elizura <98797447+Elizura@users.noreply.github.com>
2022-08-12 01:00:48 -0700

    Time: 152 ms (14.48%), Space: 14.1 MB (38.50%) - LeetHub

--- 733-flood-fill/733-flood-fill.py
+++ 733-flood-fill/733-flood-fill.py
@@ -1,18 +1,19 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        seen = set()
+        seen = set()       
         a = image[sr][sc]
-        def c(image, sr, sc, color):
+        def c(image, sr, sc, color, seen):
+            
             if sr >= 0 and sr < len(image) and sc >= 0 and sc < len(image[0]) and image[sr][sc] == a and (sr, sc) not in seen :
                 image[sr][sc] = color
                 seen.add((sr, sc))
-                c(image, sr + 1, sc, color)
-                c(image, sr - 1, sc, color)
-                c(image, sr, sc + 1, color)
-                c(image, sr, sc - 1, color)
+                c(image, sr + 1, sc, color, seen)
+                c(image, sr - 1, sc, color, seen)
+                c(image, sr, sc + 1, color, seen)
+                c(image, sr, sc - 1, color, seen)
             else:
                 return
             return image
-        return c(image, sr, sc, color)
+        return c(image, sr, sc, color, seen)
         
             
