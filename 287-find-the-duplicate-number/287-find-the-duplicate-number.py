13802a3eb0912bc03346e5c2e39cf9dc0f6e7f37
Elizura <98797447+Elizura@users.noreply.github.com>
2022-08-14 02:17:39 -0700

    Time: 1193 ms (25.28%), Space: 28 MB (30.25%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.py
@@ -1,17 +1,14 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        # [1,3,3,2,4]
-        left, right = 0, len(nums)-1
-        while left <= right:
-            mid = (left+right)//2
-            count = 0
-            for num in nums:
-                if num <= mid:
-                    count += 1
-            if count > mid:
-                right = mid - 1
-            else:
-                left = mid + 1
-
-        return left
-                    
+        slow, fast = 0, 0
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+        slow = 0
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
+            
+        return fast
