4c845e1d6721dda8cee8b447e935f8f23b343d60
Elizura <98797447+Elizura@users.noreply.github.com>
2022-08-17 15:01:49 -0700

    Time: 45 ms (72.96%), Space: 13.9 MB (60.97%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.py
+++ 101-symmetric-tree/101-symmetric-tree.py
@@ -6,12 +6,36 @@
 #         self.right = right
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        def check(rootleft, rootright):
-            if not rootleft and not rootright:
-                return True
-            if not rootleft or not rootright:
+        if not root: return True        
+        left = deque()
+        right = deque()
+        left.append(root.left)
+        right.append(root.right)
+        while left and right:            
+            a = left.popleft()
+            b = right.popleft()
+            if a and b:
+                if a.val != b.val:
+                    return False
+                left.append(a.left)
+                left.append(a.right)
+                right.append(b.right)
+                right.append(b.left)
+            elif not a and not b:
+                pass
+            else:
                 return False
-            return (rootleft.val == rootright.val) and (check(rootleft.left, rootright.right)) and (check(rootleft.right, rootright.left))
+        return not left and not right
         
-        if not root: return True
-        return check(root.left, root.right)
+        
+        
+        
+#         def check(rootleft, rootright):
+#             if not rootleft and not rootright:
+#                 return True
+#             if not rootleft or not rootright:
+#                 return False
+#             return (rootleft.val == rootright.val) and (check(rootleft.left, rootright.right)) and (check(rootleft.right, rootright.left))
+        
+#         if not root: return True
+#         return check(root.left, root.right)
