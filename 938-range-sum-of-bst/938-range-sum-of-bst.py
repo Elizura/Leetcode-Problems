3195f41ba9d4ae1338d9522c38878923a73c3e2c
Elizura <98797447+Elizura@users.noreply.github.com>
2022-08-18 14:08:25 -0700

    Time: 273 ms (77.75%), Space: 23 MB (91.87%) - LeetHub

--- 938-range-sum-of-bst/938-range-sum-of-bst.py
+++ 938-range-sum-of-bst/938-range-sum-of-bst.py
@@ -6,13 +6,21 @@
 #         self.right = right
 class Solution:
     def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
-        ans = 0
-        a = deque()
-        a.append(root)
-        while a:
-            f = a.popleft()
-            if low <= f.val and f.val <= high: ans += f.val
-            if f.left: a.append(f.left)
-            if f.right: a.append(f.right)            
-        return ans
+        ans = [0]
+        def dfs(node, low, high):
+            if not node: return 0
+            if low <= node.val <= high: ans[0] += node.val
+            dfs(node.left, low, high)
+            dfs(node.right, low, high)
+            return ans[0]
+        return dfs(root, low, high)
+        # ans = 0
+        # a = deque()
+        # a.append(root)
+        # while a:
+        #     f = a.popleft()
+        #     if low <= f.val and f.val <= high: ans += f.val
+        #     if f.left: a.append(f.left)
+        #     if f.right: a.append(f.right)            
+        # return ans
         
