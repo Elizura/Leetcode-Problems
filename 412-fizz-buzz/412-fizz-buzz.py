587e5e23bcf37726fcccd3ad6d9283c109ce4143
Elizura <98797447+Elizura@users.noreply.github.com>
2022-08-22 11:35:15 -0700

    Update 412-fizz-buzz.py

--- 412-fizz-buzz/412-fizz-buzz.py
+++ 412-fizz-buzz/412-fizz-buzz.py
@@ -1,13 +1,22 @@
-class Solution:
-    def fizzBuzz(self, n: int) -> List[str]:
-        a=[]
-        for i in range (1,n+1):
-            if i%3==0 and i%5==0:
-                a.append('FizzBuzz')
-            elif i%3==0:
-                a.append('Fizz')
-            elif i%5==0:
-                a.append('Buzz')
-            else:
-                a.append(str(i))
-        return a
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> v;        
+        for (int i = 1; i <= n; i++){
+            if (i % 3  == 0 && i % 5 == 0){
+                v.push_back("FizzBuzz");
+            }
+            else if(i % 3 == 0){
+                v.push_back("Fizz");
+            }
+            else if(i % 5 == 0){
+                v.push_back("Buzz");
+            }
+            else{
+                v.push_back(to_string(i));
+            }
+        }
+        return v;
+        
+    }
+};
