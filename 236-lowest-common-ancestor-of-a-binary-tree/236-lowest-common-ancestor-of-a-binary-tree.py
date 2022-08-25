9a22963cf11b81de1ee9c5a0b5a554a0143e092a
Elizura <98797447+Elizura@users.noreply.github.com>
2022-08-25 12:52:08 -0700

    Time: 136 ms (24.90%), Space: 26.1 MB (83.83%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
@@ -7,59 +7,70 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        def dfs(node, temp):            
-            if not node:
-                return
-            if node.val == target.val:
-                path.append(temp + [node])                
-                return
+        def dfs(node):
+            if not node: return
+            if node.val == p.val or node.val == q.val:
+                return node
+            l = dfs(node.left)
+            r = dfs(node.right)
+            if not l and not r: return
+            if l and r: return node
+            return l if l else r
+        return dfs(root)
             
-            temp.append(node)
-            dfs(node.left, temp)
-            dfs(node.right, temp)
-            # print([r.val for r in temp])
-            temp.pop()
-            
-            
-        path = []
-        target = p
-        dfs(root, [])
-        
-        target = q
-        dfs(root, [])
-                
-        path_for_p = path[0]
-        path_for_q = path[1]
-        
-        for i in range(min(len(path_for_p), len(path_for_q))):
-            if path_for_p[i] != path_for_q[i]:
-                return path_for_q[i-1]
-        
-        if len(path_for_p) < len(path_for_q):
-            return path_for_p[-1]
-        return path_for_q[-1]
-        
-#         a = []
-#         def dfs(node, target):            
-#             if not node: return False
-#             a.append([node.val])
+#         def dfs(node, temp):            
+#             if not node:
+#                 return
 #             if node.val == target.val:
-#                 return True                         
-#             if dfs(node.left, target) or dfs(node.right, target):  
-#                 return True                        
-#             a.pop()
-#             return False    
-                                
-#         if not root: return []
+#                 path.append(temp + [node])                
+#                 return
+            
+#             temp.append(node)
+#             dfs(node.left, temp)
+#             dfs(node.right, temp)
+#             # print([r.val for r in temp])
+#             temp.pop()
+            
+            
+#         path = []
+#         target = p
+#         dfs(root, [])
         
-#         dfs(root, q)
-#         dfs(root, p)        
-#         print(a)
-#         # print(path_p, path_q)
-#         # i = 0
-#         # while i < len(a):
-#         #     if a[i] != b[i]:
-#         #         return TreeNode(a[i - 1])
-#         #     i += 1
+#         target = q
+#         dfs(root, [])
+                
+#         path_for_p = path[0]
+#         path_for_q = path[1]
+        
+#         for i in range(min(len(path_for_p), len(path_for_q))):
+#             if path_for_p[i] != path_for_q[i]:
+#                 return path_for_q[i-1]
+        
+#         if len(path_for_p) < len(path_for_q):
+#             return path_for_p[-1]
+#         return path_for_q[-1]
+        
+# #         a = []
+# #         def dfs(node, target):            
+# #             if not node: return False
+# #             a.append([node.val])
+# #             if node.val == target.val:
+# #                 return True                         
+# #             if dfs(node.left, target) or dfs(node.right, target):  
+# #                 return True                        
+# #             a.pop()
+# #             return False    
+                                
+# #         if not root: return []
+        
+# #         dfs(root, q)
+# #         dfs(root, p)        
+# #         print(a)
+# #         # print(path_p, path_q)
+# #         # i = 0
+# #         # while i < len(a):
+# #         #     if a[i] != b[i]:
+# #         #         return TreeNode(a[i - 1])
+# #         #     i += 1
                             
             
