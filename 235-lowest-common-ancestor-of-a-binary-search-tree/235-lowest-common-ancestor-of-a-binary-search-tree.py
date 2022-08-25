4600142fdde3adca53c5b3608546116b6e555ca3
Elizura <98797447+Elizura@users.noreply.github.com>
2022-08-25 13:55:02 -0700

    Time: 130 ms (41.33%), Space: 18.9 MB (23.20%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,16 +7,27 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        def dfs(node):
-            if not node: return
-            if node == p or node == q:
-                return node
-            l = dfs(node.left)
-            r = dfs(node.right)
-            if not l and not r: return 
-            if l and r: return node
-            return l if l else r
-        return dfs(root)
+        def dfs(node, a, b):
+            if (a.val > node.val and b.val < node.val) or (a.val < node.val and b.val > node.val): return node
+            if a.val > node.val and b.val > node.val:
+                return dfs(node.right, a, b)
+            if a.val < node.val and b.val < node.val:
+                return dfs(node.left, a, b)
+            return a if node.val == a.val else b
+        return dfs(root, p, q)
+            
+                
+            
+#         def dfs(node):
+#             if not node: return
+#             if node == p or node == q:
+#                 return node
+#             l = dfs(node.left)
+#             r = dfs(node.right)
+#             if not l and not r: return 
+#             if l and r: return node
+#             return l if l else r
+#         return dfs(root)
         
             
         
