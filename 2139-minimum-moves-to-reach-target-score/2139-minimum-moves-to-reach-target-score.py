55c10fb7042701229a0caf6959116065698f2ccd
Elizura <98797447+Elizura@users.noreply.github.com>
2022-08-26 10:53:09 -0700

    Time: 62 ms (15.44%), Space: 13.8 MB (99.05%) - LeetHub

--- 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.py
+++ 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.py
@@ -1,15 +1,25 @@
 class Solution:
     def minMoves(self, target: int, maxDoubles: int) -> int:
-        ans = 0
-        while target > 1 and maxDoubles:
-            if not target % 2:
-                target = target // 2
-                maxDoubles -= 1                
-            else:
-                target -= 1                
-            ans += 1
-        if target: return ans + target - 1
-        return ans
+            steps=0
+            while maxDoubles>0 and target>2:
+                if target & 1: 
+                    target-=1
+                    steps+=1
+                maxDoubles-=1
+                target=target>>1
+                steps+=1
+            return steps+target-1
+
+        # ans = 0
+        # while target > 1 and maxDoubles:
+        #     if not target % 2:
+        #         target = target // 2
+        #         maxDoubles -= 1                
+        #     else:
+        #         target -= 1                
+        #     ans += 1
+        # if target: return ans + target - 1
+        # return ans
 #         for i in range(len(heights) - 1):
 #             height_difference = heights[i+1] - heights[i]
 #             if height_difference <= 0:
