6bf77f0b2345c507aa7bf0140a6e2852fad1a652
Elizura <98797447+Elizura@users.noreply.github.com>
2022-08-26 03:42:17 -0700

    Time: 223 ms (12.27%), Space: 17.7 MB (61.70%) - LeetHub

--- 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.py
+++ 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.py
@@ -6,18 +6,34 @@
 #         self.right = right
 class Solution:
     def sumEvenGrandparent(self, root: TreeNode) -> int:
-        ans = 0
-        def dfs(node):
-            nonlocal ans
-            if not node: return 0
-            if node.val % 2 == 0:
+        sumv = 0
+		# Mark the parent with even parent
+        q = collections.deque([(root, 0)])
+        while q:
+            node, evenG = q.popleft()
+            if node:
                 if node.left:
-                    if node.left.left: ans += node.left.left.val
-                    if node.left.right: ans += node.left.right.val
+				    # If parent was marked as evenG, add both the children value to res
+                    if evenG:
+                        sumv += node.left.val
+                    q.append((node.left, not node.val % 2))
                 if node.right:
-                    if node.right.left: ans += node.right.left.val
-                    if node.right.right: ans += node.right.right.val
-            dfs(node.left)
-            dfs(node.right)
-        dfs(root)
-        return ans
+                    if evenG:
+                        sumv += node.right.val
+                    q.append((node.right, not node.val % 2))
+        return sumv
+        # ans = 0
+        # def dfs(node):
+        #     nonlocal ans
+        #     if not node: return 0
+        #     if node.val % 2 == 0:
+        #         if node.left:
+        #             if node.left.left: ans += node.left.left.val
+        #             if node.left.right: ans += node.left.right.val
+        #         if node.right:
+        #             if node.right.left: ans += node.right.left.val
+        #             if node.right.right: ans += node.right.right.val
+        #     dfs(node.left)
+        #     dfs(node.right)
+        # dfs(root)
+        # return ans
