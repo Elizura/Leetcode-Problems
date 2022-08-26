0b4494e50de779c1cee0437753af1d24b0b39c70
Elizura <98797447+Elizura@users.noreply.github.com>
2022-08-26 10:31:12 -0700

    Time: 65 ms (9.74%), Space: 13.8 MB (99.05%) - LeetHub

---
+++ 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.py
@@ -0,0 +1,25 @@
+class Solution:
+    def minMoves(self, target: int, maxDoubles: int) -> int:
+        ans = 0
+        while target > 1 and maxDoubles:
+            if not target % 2:
+                target = target // 2
+                maxDoubles -= 1                
+            else:
+                target -= 1                
+            ans += 1
+        if target: return ans + target - 1
+        return ans
+#         for i in range(len(heights) - 1):
+#             height_difference = heights[i+1] - heights[i]
+#             if height_difference <= 0:
+#                 continue
+#             heapq.heappush(heap, height_difference)
+#             if len(heap) > ladders: 
+#                 b = heapq.heappop(heap)
+#                 bricks -= b
+#             if bricks < 0:
+#                 return i 
+                
+#         return len(heights)-1
+        
