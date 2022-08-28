e3ba7d76887e363ba58c0582d75c84ef9a44c801
Elizura <98797447+Elizura@users.noreply.github.com>
2022-08-28 05:26:07 -0700

    Time: 1287 ms (9.19%), Space: 24 MB (66.70%) - LeetHub

--- 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.py
+++ 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.py
@@ -6,11 +6,18 @@
 #         self.right = None
 
 class Solution:
-    def getTargetCopy(self, original: TreeNode, cloned: TreeNode, target: TreeNode) -> TreeNode:        
-        def dfs(node1, node2):            
-            if node1.val == target.val: return node2 
-            l = dfs(node1.left, node2.left) if node1.left else None                                        
-            r = dfs(node1.right, node2.right) if node1.right else None        
-            return l if l else r                        
-        return dfs(original, cloned)
+    def getTargetCopy(self, original: TreeNode, cloned: TreeNode, target: TreeNode) -> TreeNode:
+        q = deque([cloned])
+        while q:
+            a = q.popleft()
+            if a.val == target.val: return a
+            if a.left: q.append(a.left)
+            if a.right: q.append(a.right)
+            
+        # def dfs(node1, node2):            
+        #     if node1.val == target.val: return node2 
+        #     l = dfs(node1.left, node2.left) if node1.left else None                                        
+        #     r = dfs(node1.right, node2.right) if node1.right else None        
+        #     return l if l else r                        
+        # return dfs(original, cloned)
         
