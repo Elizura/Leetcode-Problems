fa3ebac395d73831943905e519b550dbd94acc36
Elizura <98797447+Elizura@users.noreply.github.com>
2022-08-28 15:40:42 -0700

    Time: 1523 ms (58.15%), Space: 24.9 MB (85.72%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,18 +1,29 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        low = prices[0]
         max_profit = 0
-        if len(prices) == 1: return 0
-        l, r = 0, 1
-        while r < len(prices):
-            if prices[r] >= prices[l]:                
-                profit = prices[r] - prices[l]
-                max_profit = max(profit, max_profit)
-            else:
-                l = r
-            r += 1
+        for i in range(1, len(prices)):
+            profit = prices[i] - low
+            max_profit = max(max_profit, profit)
+            low = min(prices[i], low)
         return max_profit
             
-                
+            
+        
+        
+        # max_profit = 0
+        # if len(prices) == 1: return 0
+        # l, r = 0, 1
+        # while r < len(prices):
+        #     if prices[r] >= prices[l]:                
+        #         profit = prices[r] - prices[l]
+        #         max_profit = max(profit, max_profit)
+        #     else:
+        #         l = r
+        #     r += 1
+        # return max_profit
+            
+# Brute force solution with O(n^2) solution
 #         ans = 0
 #         for i in range(len(prices)):
 #             for j in range(i + 1, len(prices)):
