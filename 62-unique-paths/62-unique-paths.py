5f0842f4ca4704fa7bdfa9ebcda542e8c479879b
Elizura <98797447+Elizura@users.noreply.github.com>
2022-08-31 07:58:32 -0700

    Time: 66 ms (11.11%), Space: 14.2 MB (10.57%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,19 +1,20 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        def dfss(row, col, memo = {}):
-            if (row, col) in memo: return memo[(row, col)]
-            # if (col, row) in memo: return memo[(col, row)]
-            if row >= m or col >= n: return 0
-            if row == m - 1 and col == n - 1: return 1
-            r = dfss(row, col + 1)
-            l = dfss(row + 1, col)
-            memo[(row, col)] = l + r
-            # memo[(col, row)] = l + r
-            return l + r
+#         def dfss(row, col, memo = {}):
+#             if (row, col) in memo: return memo[(row, col)]            
+#             if row >= m or col >= n: return 0
+#             if row == m - 1 and col == n - 1: return 1
+#             r = dfss(row, col + 1)
+#             l = dfss(row + 1, col)
+#             memo[(row, col)] = l + r            
+#             return l + r
             
-        # def dfs(row, col):            
-        #     if row == 0 or col == 0: return 0
-        #     if row == 1 and col == 1: return 1
-        #     return dfs(row - 1, col) + dfs(row, col - 1)
-        return dfss(0, 0)
+        def dfs(row, col, memo = {}):
+            key = (row, col)
+            if key in memo: return memo[key]
+            if row == 0 or col == 0: return 0
+            if row == 1 and col == 1: return 1
+            memo[key] = dfs(row - 1, col) + dfs(row, col - 1)
+            return memo[key]
+        return dfs(m, n)
         
