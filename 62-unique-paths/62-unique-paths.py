e22139fd60c17d4d2ede985bb6ed867b7293785f
Elizura <98797447+Elizura@users.noreply.github.com>
2022-08-31 10:56:35 -0700

    Time: 60 ms (21.40%), Space: 13.9 MB (32.77%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,5 +1,15 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
+        dp[1][1] = 1
+        for i in range(m + 1):
+            for j in range(n + 1):                
+                if i + 1 <= m:
+                    dp[i + 1][j] += dp[i][j]
+                if j + 1 <= n:
+                    dp[i][j + 1] += dp[i][j]
+        print(dp)
+        return dp[-1][-1]
 #         def dfss(row, col, memo = {}):
 #             if (row, col) in memo: return memo[(row, col)]            
 #             if row >= m or col >= n: return 0
@@ -9,12 +19,12 @@ class Solution:
 #             memo[(row, col)] = l + r            
 #             return l + r
             
-        def dfs(row, col, memo = {}):
-            key = (row, col)
-            if key in memo: return memo[key]
-            if row == 0 or col == 0: return 0
-            if row == 1 and col == 1: return 1
-            memo[key] = dfs(row - 1, col) + dfs(row, col - 1)
-            return memo[key]
-        return dfs(m, n)
+        # def dfs(row, col, memo = {}):
+        #     key = (row, col)
+        #     if key in memo: return memo[key]
+        #     if row == 0 or col == 0: return 0
+        #     if row == 1 and col == 1: return 1
+        #     memo[key] = dfs(row - 1, col) + dfs(row, col - 1)
+        #     return memo[key]
+        # return dfs(m, n)
         
