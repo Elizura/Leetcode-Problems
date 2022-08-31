43cce14037ddaff1acf66d28511376e0dfe90133
Elizura <98797447+Elizura@users.noreply.github.com>
2022-08-31 12:45:45 -0700

    Time: 1394 ms (92.07%), Space: 14.2 MB (61.95%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,17 +1,25 @@
 class Solution:    
     def coinChange(self, coins: List[int], amount: int) -> int:
-        tab = [-1 for i in range(amount + 1)]
-        tab[0] = 0
-        for cell in range(len(tab)):
-            if tab[cell] != -1:
-                for coin in coins:
-                    if cell + coin < len(tab):
-                        if tab[cell + coin] != -1:
-                            if tab[cell] + 1 < tab[cell + coin]:
-                                tab[cell + coin] = tab[cell] + 1
-                        else:
-                            tab[cell + coin] = tab[cell] + 1
-        return tab[-1]
+        coins = [coin for coin in coins if coin <= amount]
+        dp = [amount+1]*(amount+1)
+        dp[0]=0
+        for a in range(1,amount + 1):
+            for coin in coins:
+                if a-coin >= 0 and dp[a-coin] +1 < dp[a]:
+                        dp[a] = dp[a-coin] +1
+        return dp[amount] if dp[amount] <amount + 1 else -1
+        # tab = [-1 for i in range(amount + 1)]
+        # tab[0] = 0
+        # for cell in range(len(tab)):
+        #     if tab[cell] != -1:
+        #         for coin in coins:
+        #             if cell + coin < len(tab):
+        #                 if tab[cell + coin] != -1:
+        #                     if tab[cell] + 1 < tab[cell + coin]:
+        #                         tab[cell + coin] = tab[cell] + 1
+        #                 else:
+        #                     tab[cell + coin] = tab[cell] + 1
+        # return tab[-1]
         # coins.sort(reverse = True)
         # ans = 0
         # for i in coins:
