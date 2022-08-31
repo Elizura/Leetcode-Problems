e2a605163329094c1a88d2521f51af2080f0aeb6
Elizura <98797447+Elizura@users.noreply.github.com>
2022-08-31 03:58:14 -0700

    Time: 53 ms (41.18%), Space: 13.9 MB (76.16%) - LeetHub

--- 2224-minimum-number-of-operations-to-convert-time/2224-minimum-number-of-operations-to-convert-time.py
+++ 2224-minimum-number-of-operations-to-convert-time/2224-minimum-number-of-operations-to-convert-time.py
@@ -1,24 +1,39 @@
 class Solution:
-    def convertTime(self, current: str, correct: str) -> int:
-        memo = {}
+    def convertTime(self, current: str, correct: str) -> int: 
+        ret = 0
+        ans = 0
+        # memo = {}
         current = 60 * int(current[:2]) + int(current[3:])        
         correct = 60 * int(correct[:2]) + int(correct[3:])
         target = correct - current
-        nums = [1, 5, 15, 60]        
-        def dfs(target):
-            if target in memo: return memo[target]
-            if target == 0: return 0
-            if target < 0: return
-            smallest = inf
-            for i in nums:            
-                rem = target - i
-                a = dfs(rem)
-                if a is not None:                    
-                    a += 1
-                    if a < smallest:                        
-                        smallest = a                        
-            memo[target] = smallest            
-            return smallest
+        nums = [60, 15, 5, 1]
+        i = 0
+        while i < len(nums):        
+            ans += nums[i]
+            if ans > target:
+                ans -= nums[i]
+                i += 1
+            else:
+                ret += 1
+                if ans == target:
+                    return ret
+        return 0
+            
+            
+#         def dfs(target):
+#             if target in memo: return memo[target]
+#             if target == 0: return 0
+#             if target < 0: return
+#             smallest = inf
+#             for i in nums:            
+#                 rem = target - i
+#                 a = dfs(rem)
+#                 if a is not None:                    
+#                     a += 1
+#                     if a < smallest:                        
+#                         smallest = a                        
+#             memo[target] = smallest            
+#             return smallest
         
-        return dfs(target)
+#         return dfs(target)
         
