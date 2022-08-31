72aa069924dab1500116b57621aebe4ef39f9c1b
Elizura <98797447+Elizura@users.noreply.github.com>
2022-08-31 12:24:32 -0700

    Time: 1761 ms (77.30%), Space: 14.3 MB (61.96%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,19 +1,38 @@
 class Solution:    
-    def coinChange(self, coins: List[int], amount: int) -> int:  
-            memo = {}
-            def dfs(coins, amount):             
-                if amount in memo: return memo[amount]
-                if amount == 0: return 0
-                if amount < 0: return None
-                smallest = -1
-                for i in coins:            
-                    rem = amount - i
-                    a = dfs(coins, rem)
-                    if a is not None:    
-                        if a > -1:
-                            a = a + 1
-                            if smallest == -1: smallest = a                                   
-                            if a < smallest: smallest = a                
-                memo[amount] = smallest
-                return smallest
-            return dfs(coins, amount)
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        tab = [-1 for i in range(amount + 1)]
+        tab[0] = 0
+        for cell in range(len(tab)):
+            if tab[cell] != -1:
+                for coin in coins:
+                    if cell + coin < len(tab):
+                        if tab[cell + coin] != -1:
+                            if tab[cell] + 1 < tab[cell + coin]:
+                                tab[cell + coin] = tab[cell] + 1
+                        else:
+                            tab[cell + coin] = tab[cell] + 1
+        return tab[-1]
+        # coins.sort(reverse = True)
+        # ans = 0
+        # for i in coins:
+        #     while amount >= i:
+        #         amount -= i
+        #         ans += 1
+        # return ans if amount == 0 else -1
+            # memo = {}
+            # def dfs(coins, amount):             
+            #     if amount in memo: return memo[amount]
+            #     if amount == 0: return 0
+            #     if amount < 0: return None
+            #     smallest = -1
+            #     for i in coins:            
+            #         rem = amount - i
+            #         a = dfs(coins, rem)
+            #         if a is not None:    
+            #             if a > -1:
+            #                 a = a + 1
+            #                 if smallest == -1: smallest = a                                   
+            #                 if a < smallest: smallest = a                
+            #     memo[amount] = smallest
+            #     return smallest
+            # return dfs(coins, amount)
