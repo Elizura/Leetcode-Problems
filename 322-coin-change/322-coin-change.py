280fcb96bceac7ea01e6f8390e0360c940dca6e6
Elizura <98797447+Elizura@users.noreply.github.com>
2022-08-31 23:14:04 -0700

    Time: 2775 ms (27.10%), Space: 43 MB (8.66%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,13 +1,13 @@
 class Solution:    
     def coinChange(self, coins: List[int], amount: int) -> int:
-        coins = [coin for coin in coins if coin <= amount]
-        dp = [amount+1]*(amount+1)
-        dp[0]=0
-        for a in range(1,amount + 1):
-            for coin in coins:
-                if a-coin >= 0 and dp[a-coin] +1 < dp[a]:
-                        dp[a] = dp[a-coin] +1
-        return dp[amount] if dp[amount] <amount + 1 else -1
+        # coins = [coin for coin in coins if coin <= amount]
+        # dp = [amount+1]*(amount+1)
+        # dp[0]=0
+        # for a in range(1,amount + 1):
+        #     for coin in coins:
+        #         if a-coin >= 0 and dp[a-coin] +1 < dp[a]:
+        #                 dp[a] = dp[a-coin] +1
+        # return dp[amount] if dp[amount] <amount + 1 else -1
         # tab = [-1 for i in range(amount + 1)]
         # tab[0] = 0
         # for cell in range(len(tab)):
@@ -44,3 +44,21 @@ class Solution:
             #     memo[amount] = smallest
             #     return smallest
             # return dfs(coins, amount)
+            
+            memo = {}
+            def dfs(coins, amount):             
+                if amount in memo: return memo[amount]
+                if amount == 0: return 0
+                if amount < 0: return inf
+                smallest = inf
+                for i in coins:
+                    smallest = min(smallest, dfs(coins, amount - i) + 1)               
+                memo[amount] = smallest
+                return smallest
+            return -1 if dfs(coins, amount) == inf else dfs(coins, amount)
+        
+        
+        
+        
+        
+        
