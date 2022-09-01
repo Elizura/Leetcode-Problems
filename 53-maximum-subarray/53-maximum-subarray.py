b05d77185dffec5c6d3a98e0cbe77050d92a99d0
Elizura <98797447+Elizura@users.noreply.github.com>
2022-09-01 02:31:34 -0700

    Time: 1826 ms (5.01%), Space: 27.9 MB (40.23%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -7,7 +7,13 @@ class Solution:
         #     ans = max(ans, temp)
         #     temp = max(temp, 0)
         # return ans
-        ans = [nums[_] for _ in range(len(nums))]        
+#         O(N) time and O(N) space
+        # ans = [nums[_] for _ in range(len(nums))]        
+        # for i in range(1, len(nums)):
+        #     ans[i] = max(nums[i], ans[i - 1] + nums[i])
+        # return max(ans)
+        ans = nums[0]
         for i in range(1, len(nums)):
-            ans[i] = max(nums[i], ans[i - 1] + nums[i])
-        return max(ans)
+            nums[i] = max(nums[i], nums[i] + nums[i - 1])
+            ans = max(ans, nums[i])
+        return ans
