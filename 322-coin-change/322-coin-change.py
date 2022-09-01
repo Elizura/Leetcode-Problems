73e9080760c78871e542be13fe870a49b9ab7d83
Elizura <98797447+Elizura@users.noreply.github.com>
2022-08-31 23:32:42 -0700

    Time: 3569 ms (10.21%), Space: 14.3 MB (42.19%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -8,6 +8,7 @@ class Solution:
         #         if a-coin >= 0 and dp[a-coin] +1 < dp[a]:
         #                 dp[a] = dp[a-coin] +1
         # return dp[amount] if dp[amount] <amount + 1 else -1
+    
         # tab = [-1 for i in range(amount + 1)]
         # tab[0] = 0
         # for cell in range(len(tab)):
@@ -20,13 +21,7 @@ class Solution:
         #                 else:
         #                     tab[cell + coin] = tab[cell] + 1
         # return tab[-1]
-        # coins.sort(reverse = True)
-        # ans = 0
-        # for i in coins:
-        #     while amount >= i:
-        #         amount -= i
-        #         ans += 1
-        # return ans if amount == 0 else -1
+        
             # memo = {}
             # def dfs(coins, amount):             
             #     if amount in memo: return memo[amount]
@@ -45,17 +40,26 @@ class Solution:
             #     return smallest
             # return dfs(coins, amount)
             
-            memo = {}
-            def dfs(coins, amount):             
-                if amount in memo: return memo[amount]
-                if amount == 0: return 0
-                if amount < 0: return inf
-                smallest = inf
-                for i in coins:
-                    smallest = min(smallest, dfs(coins, amount - i) + 1)               
-                memo[amount] = smallest
-                return smallest
-            return -1 if dfs(coins, amount) == inf else dfs(coins, amount)
+            # memo = {}
+            # def dfs(coins, amount):             
+            #     if amount in memo: return memo[amount]
+            #     if amount == 0: return 0
+            #     if amount < 0: return inf
+            #     smallest = inf
+            #     for i in coins:
+            #         smallest = min(smallest, dfs(coins, amount - i) + 1)               
+            #     memo[amount] = smallest
+            #     return smallest
+            # return -1 if dfs(coins, amount) == inf else dfs(coins, amount)
+            
+        tab = [inf for i in range(amount + 1)]
+        tab[0] = 0
+        for cell in range(len(tab)):
+            for coin in coins:
+                if 0 <= cell - coin:
+                    tab[cell] = min(tab[cell], 1 + tab[cell - coin])
+                
+        return -1 if tab[-1] is inf else tab[-1]
         
         
         
