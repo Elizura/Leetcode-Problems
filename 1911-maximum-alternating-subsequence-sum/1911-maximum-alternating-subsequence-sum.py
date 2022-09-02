2f55ec6d1c872fabb1747aa2c6688d0b421870dc
Elizura <98797447+Elizura@users.noreply.github.com>
2022-09-02 06:43:39 -0700

    Time: 4328 ms (23.26%), Space: 381.1 MB (30.41%) - LeetHub

--- 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.py
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.py
@@ -1,20 +1,23 @@
 class Solution:
     def maxAlternatingSum(self, nums: List[int]) -> int:
-        odd, even = 0, 0
-        for i in range(0, len(nums)):
-            odd = max(even + nums[i], odd)
-            even = max(odd - nums[i], even)
-        return odd
-#         def dfs(index, odd):
-#             if index >= len(nums):
-#                 return 0
+        # odd, even = 0, 0
+        # for i in range(0, len(nums)):
+        #     odd = max(even + nums[i], odd)
+        #     even = max(odd - nums[i], even)
+        # return odd
+        def dfs(index, odd, memo = {}):
+            if (index, odd) in memo: return memo[(index, odd)]
+            if index >= len(nums):
+                return 0
             
-#             if odd:
-#                 return max(dfs(index + 1, not odd) + nums[index], dfs(index + 1, odd))
-#             else:
-#                 return max(dfs(index + 1, not odd) - nums[index], dfs(index + 1, odd))
+            if odd:
+                memo[(index, odd)] = max(dfs(index + 1, not odd, memo) + nums[index], dfs(index + 1, odd, memo))
+                return memo[(index, odd)]
+            else:
+                memo[(index, odd)] = max(dfs(index + 1, not odd, memo) - nums[index], dfs(index + 1, odd, memo))
+                return memo[(index, odd)]
             
-#         return dfs(0, 1)
+        return dfs(0, 1)
 #         def dfs(index, even):
 #             if index >= len(nums): return 0              
 #             if even:
