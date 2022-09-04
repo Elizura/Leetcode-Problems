772c2e75aab70319b3dbcc758e6baa0f83aa7038
Elizura <98797447+Elizura@users.noreply.github.com>
2022-09-03 22:46:48 -0700

    Time: 5658 ms (5.50%), Space: 381.1 MB (30.20%) - LeetHub

--- 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.py
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.py
@@ -1,18 +1,38 @@
 class Solution:
     def maxAlternatingSum(self, nums: List[int]) -> int:
-        odd, even = 0, 0
-        for i in range(0, len(nums)):
-            odd = max(even + nums[i], odd)
-            even = max(odd - nums[i], even)
-        return odd
-#         def dfs(index, odd):
+        def dfs(index, even, memo = {}):
+            if (index, even) in memo: return memo[(index, even)]
+            if index >= len(nums): return 0
+            if even:
+                a = max(nums[index] + dfs(index + 1, not even), dfs(index + 1, even))
+            if not even:
+                        a = max(-nums[index] + dfs(index + 1, not even), dfs(index + 1, even))
+            memo[(index, even)] = a
+            return a
+                        
+        return dfs(0, True)
+            
+            
+            
+            
+            
+            
+        # odd, even = 0, 0
+        # for i in range(0, len(nums)):
+        #     odd = max(even + nums[i], odd)
+        #     even = max(odd - nums[i], even)
+        # return odd
+#         def dfs(index, odd, memo = {}):
+#             if (index, odd) in memo: return memo[(index, odd)]
 #             if index >= len(nums):
 #                 return 0
             
 #             if odd:
-#                 return max(dfs(index + 1, not odd) + nums[index], dfs(index + 1, odd))
+#                 memo[(index, odd)] = max(dfs(index + 1, not odd, memo) + nums[index], dfs(index + 1, odd, memo))
+#                 return memo[(index, odd)]
 #             else:
-#                 return max(dfs(index + 1, not odd) - nums[index], dfs(index + 1, odd))
+#                 memo[(index, odd)] = max(dfs(index + 1, not odd, memo) - nums[index], dfs(index + 1, odd, memo))
+#                 return memo[(index, odd)]
             
 #         return dfs(0, 1)
 #         def dfs(index, even):
