fccd0ce88f5cadef53d1539ee33bf4e54f9d948c
Elizura <98797447+Elizura@users.noreply.github.com>
2022-09-04 09:08:44 -0700

    Time: 1282 ms (79.41%), Space: 14.2 MB (40.87%) - LeetHub

--- 673-number-of-longest-increasing-subsequence/673-number-of-longest-increasing-subsequence.py
+++ 673-number-of-longest-increasing-subsequence/673-number-of-longest-increasing-subsequence.py
@@ -1,43 +1,44 @@
 class Solution:
     def findNumberOfLIS(self, nums: List[int]) -> int:
-        # cnt = [1 for i in range(len(nums))]
-        # tab = [1 for i in range(len(nums))]
-        # for i in range(1, len(tab)):
-        #     for j in range(i):
-        #         if nums[i] > nums[j]:
-        #             if tab[j] + 1 == tab[i]:
-        #                 cnt[i] += cnt[j]
-        #             tab[i] = max(tab[j] + 1, tab[i])
-        # print(cnt)
-        # a = max(tab) 
-        # ans = 0
-        # for i in range(len(tab)):
-        #     if tab[i] == a:
-        #         ans += cnt[i]
-        # return ans
-    
-        dp = [1] * len(nums)
-        cnt = [1] * len(nums)
-        
-        maxSoFar = 1
-        
-        for i in range(len(nums)):
+        cnt = [1 for i in range(len(nums))]
+        tab = [1 for i in range(len(nums))]
+        for i in range(1, len(tab)):
             for j in range(i):
-                if nums[j] < nums[i]:
-                    if dp[i] == dp[j] + 1:
+                if nums[i] > nums[j]:
+                    if tab[j] + 1 == tab[i]:
                         cnt[i] += cnt[j]
+                    elif tab[i] < tab[j] + 1:
+                        tab[i] = tab[j] + 1
+                        cnt[i] = cnt[j]        
+        a = max(tab) 
+        ans = 0
+        for i in range(len(tab)):
+            if tab[i] == a:
+                ans += cnt[i]
+        return ans
+    
+#         dp = [1] * len(nums)
+#         cnt = [1] * len(nums)
+        
+#         maxSoFar = 1
+        
+#         for i in range(len(nums)):
+#             for j in range(i):
+#                 if nums[j] < nums[i]:
+#                     if dp[i] == dp[j] + 1:
+#                         cnt[i] += cnt[j]
                     
-                    elif dp[i] < dp[j] + 1:
-                        dp[i] = dp[j] + 1
-                        cnt[i] = cnt[j]
-                    # elif dp[i] == dp[j] + 1:
-                    #     cnt[i] += cnt[j]
+                    
+#                     elif dp[i] < dp[j] + 1:
+#                         dp[i] = dp[j] + 1
+#                         cnt[i] = cnt[j]
+
                         
-            maxSoFar = max(maxSoFar, dp[i])
+#             maxSoFar = max(maxSoFar, dp[i])
             
-        nos = 0
-        for i in range(len(nums)):
-            if dp[i] == maxSoFar:
-                nos += cnt[i]
+#         nos = 0
+#         for i in range(len(nums)):
+#             if dp[i] == maxSoFar:
+#                 nos += cnt[i]
                 
-        return nos
+#         return nos
