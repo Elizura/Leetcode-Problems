e061d75ebebbd04cbc2b7cbde8e807ab17e28208
Elizura <98797447+Elizura@users.noreply.github.com>
2022-09-05 13:22:26 -0700

    Time: 5818 ms (8.84%), Space: 485.9 MB (5.01%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
@@ -1,25 +1,25 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:                
-        sem = sum(nums)
-        target = sem/2
-        if int(target) != target: return False
-        a = [False for i in range(int(target) + 1)]
-        a[0] = True
-        for num in nums:
-            for i in range(len(a) -1, num - 1, -1):
-                a[i] = a[i] or a[i - num]        
-        return a[-1]
-        # seen = set([0])
         # sem = sum(nums)
         # target = sem/2
-        # if int(target) != target: return False        
-        # def dfs(index, aim, memo = {}):
-        #     if aim in memo: return memo[aim]
-        #     if index >= len(nums): return False
-        #     if aim == 0: return True
-        #     memo[aim] = dfs(index + 1, aim - nums[index]) or dfs(index + 1, aim) 
-        #     return memo[aim]
-        # return dfs(0, target)
+        # if int(target) != target: return False
+        # a = [False for i in range(int(target) + 1)]
+        # a[0] = True
+        # for num in nums:
+        #     for i in range(len(a) -1, num - 1, -1):
+        #         a[i] = a[i] or a[i - num]        
+        # return a[-1]
+        # seen = set([0])
+        sem = sum(nums)
+        target = sem/2
+        if int(target) != target: return False        
+        def dfs(index, aim, memo = {}):
+            if (index, aim) in memo: return memo[(index, aim)]
+            if index >= len(nums): return False
+            if aim == 0: return True
+            memo[(index, aim)] = dfs(index + 1, aim - nums[index]) or dfs(index + 1, aim) 
+            return memo[(index, aim)]
+        return dfs(0, target)
         # seen = set([0])
         # sem = sum(nums)
         # target = sem/2
