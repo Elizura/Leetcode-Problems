cf78b970060675291d9d870d51c538b414a15c82
Elizura <98797447+Elizura@users.noreply.github.com>
2022-09-05 12:17:04 -0700

    Time: 1483 ms (56.96%), Space: 13.9 MB (97.55%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
@@ -1,15 +1,35 @@
 class Solution:
-    def canPartition(self, nums: List[int]) -> bool:
-        seen = set([0])
+    def canPartition(self, nums: List[int]) -> bool:                
         sem = sum(nums)
         target = sem/2
         if int(target) != target: return False
-        for i in range(len(nums) - 1, -1, -1):
-            temp = set()
-            # temp = seen
-            for j in seen:               
-                temp.add(j + nums[i])
-                temp.add(j)
-            seen = temp
-        if target in seen: return True
-        return False
+        a = [False for i in range(int(target) + 1)]
+        a[0] = True
+        for num in nums:
+            for i in range(len(a) -1, num - 1, -1):
+                a[i] = a[i] or a[i - num]        
+        return a[-1]
+        # seen = set([0])
+        # sem = sum(nums)
+        # target = sem/2
+        # if int(target) != target: return False        
+        # def dfs(index, aim, memo = {}):
+        #     if aim in memo: return memo[aim]
+        #     if index >= len(nums): return False
+        #     if aim == 0: return True
+        #     memo[aim] = dfs(index + 1, aim - nums[index]) or dfs(index + 1, aim) 
+        #     return memo[aim]
+        # return dfs(0, target)
+        # seen = set([0])
+        # sem = sum(nums)
+        # target = sem/2
+        # if int(target) != target: return False
+        # for i in range(len(nums) - 1, -1, -1):
+        #     temp = set()
+        #     # temp = seen
+        #     for j in seen:               
+        #         temp.add(j + nums[i])
+        #         temp.add(j)
+        #     seen = temp
+        # if target in seen: return True
+        # return False
