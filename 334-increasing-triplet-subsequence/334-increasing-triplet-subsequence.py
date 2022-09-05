ecf09c6313f844c3bd764a55c4049b3d817ac87a
Elizura <98797447+Elizura@users.noreply.github.com>
2022-09-05 02:08:31 -0700

    Time: 598 ms (91.37%), Space: 24.5 MB (95.36%) - LeetHub

--- 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.py
+++ 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.py
@@ -1,24 +1,37 @@
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
-        _min = [nums[0]]
-        _max = [nums[-1] for i in range(len(nums))]
-        
-        for i in range(1, len(nums)):
-            if nums[i] < _min[-1]:
-                _min.append(nums[i])
-            else:
-                _min.append(_min[-1])
-                
-        for i in range(len(nums) - 2, -1, -1):
-            if nums[i] > _max[i + 1]:
-                _max[i] = nums[i]
-            else:
-                _max[i] = _max[i + 1]
-                
-        for i in range(len(nums)):
-            if _min[i] < nums[i] < _max[i]:
-                return True        
+        _min = nums[0]
+        _max = inf
+        for i in nums[1:]:
+            if i > _max:
+                return True
+            elif i < _min:
+                _min = i
+            elif i > _min and i < _max:
+                _max = i
         return False
+                
+        
+        
+#         _min = [nums[0]]
+#         _max = [nums[-1] for i in range(len(nums))]
+        
+#         for i in range(1, len(nums)):
+#             if nums[i] < _min[-1]:
+#                 _min.append(nums[i])
+#             else:
+#                 _min.append(_min[-1])
+                
+#         for i in range(len(nums) - 2, -1, -1):
+#             if nums[i] > _max[i + 1]:
+#                 _max[i] = nums[i]
+#             else:
+#                 _max[i] = _max[i + 1]
+                
+#         for i in range(len(nums)):
+#             if _min[i] < nums[i] < _max[i]:
+#                 return True        
+#         return False
                 
         # tab = [1 for i in range(len(nums))]
         # for i in range(1, len(tab)):
