b47ad0c97ca418e6d9d78a695d0df07eee2512a2
Elizura <98797447+Elizura@users.noreply.github.com>
2022-09-05 06:41:19 -0700

    Time: 1361 ms (17.62%), Space: 29.8 MB (38.20%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.py
+++ 518-coin-change-2/518-coin-change-2.py
@@ -13,12 +13,4 @@ class Solution:
                 else:
                     tab[i][j] = tab[i - 1][j]
         return tab[-1][-1]
-        # def dfs(amt, idx, memo = {}):
-        #     if (idx, amt) in memo: return memo[(idx, amt)]
-        #     if amt == amount: return 1
-        #     if amt > amount: return 0
-        #     if idx >= len(coins): return 0
-        #     memo[(idx, amt)] = dfs(coins[idx] + amt, idx) + dfs(amt, idx + 1)
-        #     return memo[(idx, amt)]
-        # return dfs(0, 0)
     
