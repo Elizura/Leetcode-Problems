8dea7d722bffd8ddee98e1bbfd09bdf5ca587dbb
Elizura <98797447+Elizura@users.noreply.github.com>
2022-09-06 09:09:07 -0700

    Time: 49 ms (52.39%), Space: 13.9 MB (19.62%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,10 +1,32 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        def dfs(index, amount, memo = {}):
-            if (amount, index) in memo: return memo[(amount, index)]
-            if index >= len(nums): return amount
-            rob = dfs(index + 2, amount + nums[index])
-            skip = dfs(index + 1, amount)
-            memo[(amount, index)] = max(rob, skip)
-            return memo[(amount, index)]
-        return dfs(0, 0)
+        if len(nums)==1:
+            return nums[0]
+        dp = {}
+        dp[0] = nums[0]
+        dp[1] = max(nums[0:2]) 
+        for i in range(2,len(nums)):
+            dp[i] = max(dp[i-1],nums[i]+dp[i-2])
+        
+        
+        return(max(dp.values()))
+        # take, nottake = 0 , 0 
+        # for index in range(len(nums)):
+        #     take = nottake + nums[index]
+        #     nottake = max(take, nottake)
+        # return max(take, nottake)
+    
+#         _min = 0
+#         _max = 0
+#         for i in range(len(nums)):
+#             _max = max(_min + nums[i], _max)
+#             _min = min(_min + nums[i], _min)
+#         return max(_max, _min)
+        # def dfs(index, amount, memo = {}):
+        #     if (amount, index) in memo: return memo[(amount, index)]
+        #     if index >= len(nums): return amount
+        #     rob = dfs(index + 2, amount + nums[index])
+        #     skip = dfs(index + 1, amount)
+        #     memo[(amount, index)] = max(rob, skip)
+        #     return memo[(amount, index)]
+        # return dfs(0, 0)
