131b6e0b9594704772391d7d349fd27ee16f44bc
Elizura <98797447+Elizura@users.noreply.github.com>
2022-09-06 06:42:14 -0700

    Time: 319 ms (75.97%), Space: 27.1 MB (43.14%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.py
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.py
@@ -1,30 +1,28 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-#         pref = 0
-#         left = 0
-#         ans = inf
-#         for right in range(2*len(nums)):
-#             if right < len(nums):
-#                 pref += nums[right]                         
-#             if pref > target:
-#                 pref -= nums[left]
-#                 left += 1
-#             if pref == target:                
-#                 ans = min(ans, right - left + 1)
-            
-#         return ans
+        pref = 0
+        left = 0
         ans = inf
-        sun = 0
-        l, r = 0, 0
-        while r <= len(nums) and r >= l:            
-            if sun < target:
-                if r == len(nums): break
-                sun += nums[r]
-                r += 1
-            else:
-                ans = min(ans, r - l)
-                sun -= nums[l]                
-                l += 1
-                
+        for right in range(len(nums)): 
+            pref += nums[right]
+            while pref >= target:
+                pref -= nums[left]
+                ans = min(ans, right - left + 1)
+                left += 1            
+            
         return 0 if ans is inf else ans
+#         ans = inf
+#         sun = 0
+#         l, r = 0, 0
+#         while r <= len(nums) and r >= l:            
+#             if sun < target:
+#                 if r == len(nums): break
+#                 sun += nums[r]
+#                 r += 1
+#             else:
+#                 ans = min(ans, r - l)
+#                 sun -= nums[l]                
+#                 l += 1
+                
+#         return 0 if ans is inf else ans
 
