fe2819889439f9830f6994bcd85922c9914b238a
Elizura <98797447+Elizura@users.noreply.github.com>
2022-09-07 07:11:44 -0700

    Time: 5626 ms (6.61%), Space: 381.7 MB (28.46%) - LeetHub

--- 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.py
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.py
@@ -25,24 +25,27 @@ class Solution:
         #     odd = max(even + nums[i], odd)
         #     even = max(odd - nums[i], even)
         # return odd
-        def dfs(index, odd, memo = {}):
-            if (index, odd) in memo: return memo[(index, odd)]
-            if index >= len(nums):
-                return 0
+#         def dfs(index, odd, memo = {}):
+#             if (index, odd) in memo: return memo[(index, odd)]
+#             if index >= len(nums):
+#                 return 0
             
-            if odd:
-                memo[(index, odd)] = max(dfs(index + 1, not odd, memo) + nums[index], dfs(index + 1, odd, memo))
-                return memo[(index, odd)]
-            else:
-                memo[(index, odd)] = max(dfs(index + 1, not odd, memo) - nums[index], dfs(index + 1, odd, memo))
-                return memo[(index, odd)]
-            
-        return dfs(0, 1)
-#         def dfs(index, even):
-#             if index >= len(nums): return 0              
-#             if even:
-#                 return max(nums[index] + dfs(index + 1, not even), dfs(index + 1, even))
+#             if odd:
+#                 memo[(index, odd)] = max(dfs(index + 1, not odd, memo) + nums[index], dfs(index + 1, odd, memo))
+#                 return memo[(index, odd)]
 #             else:
-#                 return max(-nums[index] + dfs(index + 1, not even), dfs(index + 1, even))
+#                 memo[(index, odd)] = max(dfs(index + 1, not odd, memo) - nums[index], dfs(index + 1, odd, memo))
+#                 return memo[(index, odd)]
             
 #         return dfs(0, 1)
+        def dfs(index, even, memo = {}):
+            if (index, even) in memo: return memo[(index, even)]
+            if index >= len(nums): return 0              
+            if even:
+                memo[(index, even)] = max(nums[index] + dfs(index + 1, not even), dfs(index + 1, even))
+                return memo[(index, even)]
+            else:
+                memo[(index, even)] = max(-nums[index] + dfs(index + 1, not even), dfs(index + 1, even))
+                return memo[(index, even)]
+            
+        return dfs(0, 1)
