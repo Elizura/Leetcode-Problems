5b7f138b3f8d39eccb10609640cb31bb347126d2
Elizura <98797447+Elizura@users.noreply.github.com>
2022-09-07 09:13:38 -0700

    Time: 2371 ms (66.17%), Space: 60.3 MB (80.54%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.py
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.py
@@ -1,12 +1,58 @@
 class Solution:
     def mostPoints(self, questions: List[List[int]]) -> int:
-        n = len(questions)
-        @lru_cache(None)
-        def dfs(i):
-            if i >= n:
-                return 0
-            return max(dfs(i+1),questions[i][0] + dfs(i+questions[i][1]+1))
-        return dfs(0)
+        tab = [i[0] for i in questions]
+        for i in range(len(questions) - 2, -1, -1):
+            if i + questions[i][-1] + 1 < len(tab):
+                tab[i] = max(questions[i][0] + tab[i + questions[i][-1] + 1], tab[i + 1])
+            else:
+                tab[i] = max(tab[i], tab[i + 1])
+        return max(tab)
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         n = len(questions)
+#         dp = [0 for _ in range(n)]
+        
+#         for i in range(n-1, -1, -1):
+#             idx = i + questions[i][1] + 1
+#             if idx < n:
+#                 dp[i] = dp[idx] + questions[i][0]
+#             else:
+#                 dp[i] = questions[i][0]
+            
+#             if i < n-1:
+#                 dp[i] = max(dp[i], dp[i+1])
+        
+#         return dp[0]
+        # n = len(questions)
+        # # @lru_cache(None)
+        # def dfs(i):
+        #     if i >= n:
+        #         return 0
+        #     return max(dfs(i+1),questions[i][0] + dfs(i+questions[i][1]+1))
+        # return dfs(0)
 #         a, b = 0, 0
 #         for i in questions:
 #             a, b = b, max(i[0])
