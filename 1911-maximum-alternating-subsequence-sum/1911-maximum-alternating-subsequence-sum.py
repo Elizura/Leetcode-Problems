51a587092524e2506d0c63825b3eb5dc7b4bd3fd
Elizura <98797447+Elizura@users.noreply.github.com>
2022-09-07 07:01:49 -0700

    Time: 2438 ms (52.71%), Space: 28.6 MB (68.54%) - LeetHub

--- 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.py
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.py
@@ -1,27 +1,30 @@
 class Solution:
     def maxAlternatingSum(self, nums: List[int]) -> int:
-        def dfs(index, even, memo = {}):
-            if (index, even) in memo: return memo[(index, even)]
-            if index >= len(nums): return 0
-            if even:
-                a = max(nums[index] + dfs(index + 1, not even), dfs(index + 1, even))
-            if not even:
-                        a = max(-nums[index] + dfs(index + 1, not even), dfs(index + 1, even))
-            memo[(index, even)] = a
-            return a
+        # a, b = 0, 0
+        # for i in nums:
+        #     a, b = b, max(i, a)
+#         def dfs(index, even, memo = {}):
+#             if (index, even) in memo: return memo[(index, even)]
+#             if index >= len(nums): return 0
+#             if even:
+#                 a = max(nums[index] + dfs(index + 1, not even), dfs(index + 1, even))
+#             if not even:
+#                         a = max(-nums[index] + dfs(index + 1, not even), dfs(index + 1, even))
+#             memo[(index, even)] = a
+#             return a
                         
-        return dfs(0, True)
+#         return dfs(0, True)
             
             
             
             
             
             
-        # odd, even = 0, 0
-        # for i in range(0, len(nums)):
-        #     odd = max(even + nums[i], odd)
-        #     even = max(odd - nums[i], even)
-        # return odd
+        odd, even = 0, 0
+        for i in range(len(nums)):
+            odd = max(even + nums[i], odd)
+            even = max(odd - nums[i], even)
+        return odd
 #         def dfs(index, odd, memo = {}):
 #             if (index, odd) in memo: return memo[(index, odd)]
 #             if index >= len(nums):
