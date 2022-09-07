97c70e5bcd40e9730bc6dbdb1156ac09bfe59b23
Elizura <98797447+Elizura@users.noreply.github.com>
2022-09-07 14:27:22 -0700

    Time: 59 ms (16.88%), Space: 13.7 MB (96.19%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,5 +1,13 @@
-# class Solution:    
-#     def climbStairs(self, n: int) -> int:
+class Solution:    
+    def climbStairs(self, n: int) -> int:
+        cur = 0
+        a, b = 0, 1
+        for i in range(1, n + 1):
+            cur = b + a
+            a = b
+            b = cur            
+        return b
+
 #         storage = {}
 #         if n <= 2:
 #             storage[n] = n 
@@ -12,10 +20,12 @@
 #                 return storage[n]
 
 
-class Solution:
-    dic = {}
-    def climbStairs(self, n: int) -> int:        
-        if n in self.dic: return self.dic[n]
-        if n <= 1: return 1        
-        self.dic[n] = self.climbStairs(n - 1) + self.climbStairs(n - 2)
-        return self.dic[n]        
+# class Solution:
+#     dic = {}
+#     def climbStairs(self, n: int) -> int:        
+#         if n in self.dic: return self.dic[n]
+#         if n <= 1: return 1        
+#         self.dic[n] = self.climbStairs(n - 1) + self.climbStairs(n - 2)
+#         return self.dic[n]        
+
+
