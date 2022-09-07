e76b79f5cde7a02065629790f75c6c26fbc0bfcf
Elizura <98797447+Elizura@users.noreply.github.com>
2022-09-07 06:56:36 -0700

    Time: 310 ms (95.59%), Space: 16.5 MB (74.61%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.py
@@ -10,26 +10,4 @@ class Solution:
             if pref in dic:
                 dic[pref] += 1
             if pref not in dic: dic[pref] = 1
-        return ans
-        
-        
-        # def dfs(index, total):            
-#             if index >= len(nums): return 0
-#             total += nums[index]
-#             if k < 0:
-#                 if total < k: return 0
-#             if k > 0:
-#                 if total > k: return 0
-#             if total == k: return 1            \U0001f60a\U0001f60a\U0001f60a\U0001f60a\U0001f60a\U0001f60a\U0001f60a
-                        
-#             return dfs(index + 1, total)
-        
-#         ans = 0
-#         for i in range(len(nums)):
-#             a = dfs(i, 0)
-#             ans += a
-            
-#         return ans
-        
-            
-        
+        return ans            
