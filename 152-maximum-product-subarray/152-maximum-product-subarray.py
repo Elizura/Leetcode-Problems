342f3cc8b81cee2eb0073154cca0be6a62b8c722
Elizura <98797447+Elizura@users.noreply.github.com>
2022-09-07 07:26:39 -0700

    Time: 123 ms (61.40%), Space: 15.7 MB (7.15%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.py
+++ 152-maximum-product-subarray/152-maximum-product-subarray.py
@@ -1,22 +1,22 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        minProd = maxProd = result = nums[0]
-        for n in nums[1:]:
-            t = [n, minProd*n, maxProd*n]
-            minProd, maxProd = min(t), max(t)     
-            result = max(result, maxProd)            
-        return result
-        # maks = [i for i in nums]
-        # mean = [i for i in nums]
-        # maks[0] = mean[0] = nums[0]
-        # for i in range(1, len(nums)):
-        #     if nums[i] > 0:
-        #         maks[i] = max(nums[i]*maks[i - 1], nums[i])
-        #         mean[i] = min(nums[i]*mean[i - 1], nums[i])
-        #     else:
-        #         maks[i] = max(nums[i]*mean[i - 1], nums[i])
-        #         mean[i] = min(nums[i]*maks[i - 1], nums[i])       
-        # return max(maks)
+        # minProd = maxProd = result = nums[0]
+        # for n in nums[1:]:
+        #     t = [n, minProd*n, maxProd*n]
+        #     minProd, maxProd = min(t), max(t)     
+        #     result = max(result, maxProd)            
+        # return result
+        maks = [i for i in nums]
+        mean = [i for i in nums]
+        maks[0] = mean[0] = nums[0]
+        for i in range(1, len(nums)):
+            if nums[i] > 0:
+                maks[i] = max(nums[i]*maks[i - 1], nums[i])
+                mean[i] = min(nums[i]*mean[i - 1], nums[i])
+            else:
+                maks[i] = max(nums[i]*mean[i - 1], nums[i])
+                mean[i] = min(nums[i]*maks[i - 1], nums[i])       
+        return max(maks)
         # max_ = -inf
         # j = 0
         # while j < len(nums):
