715aeb9773550d89b150ee513dfc2f2424f49fd3
Elizura <98797447+Elizura@users.noreply.github.com>
2022-09-10 05:33:55 -0700

    Time: 48 ms (49.15%), Space: 13.8 MB (61.96%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.py
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.py
@@ -1,4 +1,15 @@
 class Solution:
     def numTrees(self, n: int) -> int:
-        return (math.factorial(2*n)) // (math.factorial(n + 1) * math.factorial(n))
+        memo = [1 for i in range(n + 1)]
+        for node in range(2, len(memo)):
+            total = 0
+            for root in range(1, node + 1):
+                left = root - 1                
+                right = node - root
+                total += memo[left] * memo[right]
+            memo[node] = total
+        return memo[-1]
+                
+            
+        # return (math.factorial(2*n)) // (math.factorial(n + 1) * math.factorial(n))
         
