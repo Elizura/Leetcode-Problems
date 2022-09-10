3a3ec66bebfe6e18897c7e282ded9d0270e2b993
Elizura <98797447+Elizura@users.noreply.github.com>
2022-09-10 01:15:59 -0700

    Time: 66 ms (92.35%), Space: 15.1 MB (69.34%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,19 +1,25 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        def dfs(idx, bought):
-            if (idx, bought) in memo : return memo[(idx, bought)]
-            if idx > len(prices) - 1: return 0
-            if idx == len(prices) - 1:
-                if bought:
-                    return prices[-1]
-                else:
-                    return 0
-            if not bought:
-                memo[(idx, bought)] = max(-prices[idx] + dfs(idx + 1, True), dfs(idx + 1, False))
-                return memo[(idx, bought)]
-            else:
-                memo[(idx, bought)] = max(prices[idx] + dfs(idx + 1, False), dfs(idx + 1, True))
-                return memo[(idx, bought)]
-        memo = {}
-        memo[(len(prices) - 1, True)] = prices[-1]
-        return dfs(0, False)
+        ans = 0
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i - 1]:
+                ans += prices[i] - prices[i - 1]            
+        return ans
+                
+        # def dfs(idx, bought):
+        #     if (idx, bought) in memo : return memo[(idx, bought)]
+        #     if idx > len(prices) - 1: return 0
+        #     if idx == len(prices) - 1:
+        #         if bought:
+        #             return prices[-1]
+        #         else:
+        #             return 0
+        #     if not bought:
+        #         memo[(idx, bought)] = max(-prices[idx] + dfs(idx + 1, True), dfs(idx + 1, False))
+        #         return memo[(idx, bought)]
+        #     else:
+        #         memo[(idx, bought)] = max(prices[idx] + dfs(idx + 1, False), dfs(idx + 1, True))
+        #         return memo[(idx, bought)]
+        # memo = {}
+        # memo[(len(prices) - 1, True)] = prices[-1]
+        # return dfs(0, False)
