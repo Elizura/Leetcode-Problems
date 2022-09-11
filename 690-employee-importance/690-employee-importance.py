2b3dbb8a374ddcc799fc59470e2bd1bf1e3136f3
Elizura <98797447+Elizura@users.noreply.github.com>
2022-09-11 05:53:24 -0700

    Time: 291 ms (21.70%), Space: 15.7 MB (29.13%) - LeetHub

--- 690-employee-importance/690-employee-importance.py
+++ 690-employee-importance/690-employee-importance.py
@@ -9,16 +9,21 @@ class Employee:
 
 class Solution:
     def getImportance(self, employees: List['Employee'], id: int) -> int:
-        seen = set()
-        ans = 0
+        dic = {_.id: _ for _ in employees}
         def dfs(idx):
-            nonlocal ans
-            if idx in seen: return 0
-            seen.add(idx)
-            for i in employees:
-                if i.id == idx:
-                    ans += i.importance
-                    for j in i.subordinates:
-                        dfs(j)
-        dfs(id)
-        return ans
+            employee = dic[idx]
+            return employee.importance + sum([dfs(i) for i in employee.subordinates])
+        return dfs(id)
+        # seen = set()
+        # ans = 0
+        # def dfs(idx):
+        #     nonlocal ans
+        #     if idx in seen: return 0
+        #     seen.add(idx)
+        #     for i in employees:
+        #         if i.id == idx:
+        #             ans += i.importance
+        #             for j in i.subordinates:
+        #                 dfs(j)
+        # dfs(id)
+        # return ans
