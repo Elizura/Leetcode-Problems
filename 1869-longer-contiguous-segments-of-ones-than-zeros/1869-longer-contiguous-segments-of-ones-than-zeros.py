3fa01a4a8d1064c7a9c9bfa025102d0b877d2008
Elizura <98797447+Elizura@users.noreply.github.com>
2022-09-12 05:12:09 -0700

    Time: 39 ms (83.36%), Space: 13.8 MB (68.52%) - LeetHub

--- 1869-longer-contiguous-segments-of-ones-than-zeros/1869-longer-contiguous-segments-of-ones-than-zeros.py
+++ 1869-longer-contiguous-segments-of-ones-than-zeros/1869-longer-contiguous-segments-of-ones-than-zeros.py
@@ -1,25 +1,39 @@
 class Solution:
     def checkZeroOnes(self, s: str) -> bool:
-        def ones(s):
-            pref = 0
-            ans = 0
-            for i in s:
-                if int(i) == 1:
-                    pref += 1
-                    ans = max(ans, pref)
-                else:
-                    pref = 0
-            return ans
-        def zeros(s):
-            pref = 0
-            ans = 0
-            for i in s:
-                if int(i) == 0:
-                    pref += 1
-                    ans = max(ans, pref)
-                else:
-                    pref = 0
-            return ans
-        wans = ones(s)
-        theros = zeros(s)
-        return wans > theros
+        maxonecount = 0
+        onecount = 0
+        maxzerocount = 0
+        zerocount = 0
+        for char in s:
+            if char == '1':
+                onecount += 1
+                maxonecount = max(maxonecount, onecount)
+                zerocount = 0
+            else:
+                zerocount += 1
+                maxzerocount = max(maxzerocount, zerocount)
+                onecount = 0
+        return maxonecount > maxzerocount
+        # def ones(s):
+        #     pref = 0
+        #     ans = 0
+        #     for i in s:
+        #         if int(i) == 1:
+        #             pref += 1
+        #             ans = max(ans, pref)
+        #         else:
+        #             pref = 0
+        #     return ans
+        # def zeros(s):
+        #     pref = 0
+        #     ans = 0
+        #     for i in s:
+        #         if int(i) == 0:
+        #             pref += 1
+        #             ans = max(ans, pref)
+        #         else:
+        #             pref = 0
+        #     return ans
+        # wans = ones(s)
+        # theros = zeros(s)
+        # return wans > theros
