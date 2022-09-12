91235b21b6df3727f1ca5c8df2e334962720fb25
Elizura <98797447+Elizura@users.noreply.github.com>
2022-09-12 07:40:17 -0700

    Time: 18642 ms (5.01%), Space: 31.7 MB (5.04%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -1,13 +1,17 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        l = len(nums)
-        stack = [0] + [9999] * (l - 2)
-        for i in range(l - 1):
-            for j in range(i + 1, i + nums[i] + 1):
-                if i + nums[i] >= l - 1:
-                    return stack[i] + 1
-                stack[j] = min(stack[j], stack[i] + 1)
-        return stack[l - 2]
+        leng = len(nums)
+        if leng == 1: return 0
+        @cache
+        def dfs(loc):
+            if loc + nums[loc] >= leng-1: return 1
+            elif loc == leng-1: return 0
+            step = inf			
+            for i in range(1, nums[loc]+1):
+                sub = 1 + dfs(loc+i)
+                step = min(sub, step)
+            return step
+        return dfs(0)
     
         # leng = len(nums)
         # if leng == 1: return 0
