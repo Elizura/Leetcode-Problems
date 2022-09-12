a3c7ea4aa176e6542d053257b316ba30c47ece4e
Elizura <98797447+Elizura@users.noreply.github.com>
2022-09-12 12:24:51 -0700

    Time: 172 ms (45.04%), Space: 15.8 MB (54.20%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.py
+++ 64-minimum-path-sum/64-minimum-path-sum.py
@@ -1,13 +1,24 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        # @cache
-        def dfs(row, col):
-            if (row, col) in dp: return dp[(row, col)]
-            if row >= len(grid) or col >= len(grid[0]): return inf
-            if row == len(grid) - 1 and col == len(grid[0]) - 1: return grid[-1][-1]
-            down = dfs(row + 1, col) + grid[row][col]
-            right = dfs(row, col + 1) + grid[row][col]
-            dp[(row, col)] = min(down, right)
-            return min(down, right)
-        dp = {}
-        return dfs(0, 0)
+        row, col = len(grid), len(grid[0])
+        dp = [[0] * col for i in range(row)]        
+        dp[0][0] = grid[0][0]
+        for j in range(1, col):
+            dp[0][j] = dp[0][j-1] + grid[0][j]
+        for i in range(1, row):   
+            dp[i][0] = dp[i-1][0]+ grid[i][0]
+        for i in range(1,row):
+            for j in range(1, col):               
+                    dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j]
+        return dp[-1][-1]
+        # # @cache
+        # def dfs(row, col):
+        #     if (row, col) in dp: return dp[(row, col)]
+        #     if row >= len(grid) or col >= len(grid[0]): return inf
+        #     if row == len(grid) - 1 and col == len(grid[0]) - 1: return grid[-1][-1]
+        #     down = dfs(row + 1, col) + grid[row][col]
+        #     right = dfs(row, col + 1) + grid[row][col]
+        #     dp[(row, col)] = min(down, right)
+        #     return min(down, right)
+        # dp = {}
+        # return dfs(0, 0)
