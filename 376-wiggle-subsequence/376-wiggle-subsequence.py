baef99d015b027092816a27020eaa66c3c35e22f
Elizura <98797447+Elizura@users.noreply.github.com>
2022-09-13 06:37:16 -0700

    Time: 58 ms (50.73%), Space: 14 MB (35.25%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.py
+++ 376-wiggle-subsequence/376-wiggle-subsequence.py
@@ -1,13 +1,30 @@
 class Solution:
     def wiggleMaxLength(self, nums: List[int]) -> int:
-        ans = 2
-        N = len(nums)
-        dp = [nums[i] - nums[i - 1] for i in range(1, N) if nums[i] - nums[i - 1] != 0]
-        if not dp: return 1
-        for i in range(1, len(dp)):
-            if dp[i - 1] > 0 and dp[i] < 0 or dp[i - 1] < 0 and dp[i] > 0:
-                ans += 1
-        return ans
+        if not nums:
+            return 0
+        
+        length = 1
+        up = None
+        for i in range(1, len(nums)):
+            if nums[i] > nums[i - 1] and up != True:
+                length += 1
+                up = True
+            if nums[i] < nums[i - 1] and up != False:
+                length += 1
+                up = False
+        return length
+        
+        # #O(n) TC and O(N) SC
+        # ans = 2
+        # N = len(nums)
+        # dp = [nums[i] - nums[i - 1] for i in range(1, N) if nums[i] - nums[i - 1] != 0]
+        # if not dp: return 1
+        # for i in range(1, len(dp)):
+        #     if dp[i - 1] > 0 and dp[i] < 0 or dp[i - 1] < 0 and dp[i] > 0:
+        #         ans += 1
+        # return ans
+    
+        #O(N^2) time and O(1) space
         # ans = 1
         # pt = 1
         # N = len(nums)
