71f3d2dd08d26eafa7ca6cc5ff6ff8a83ddb7cb4
Elizura <98797447+Elizura@users.noreply.github.com>
2022-09-13 12:21:00 -0700

    Time: 418 ms (5.30%), Space: 14.5 MB (31.04%) - LeetHub

---
+++ 605-can-place-flowers/605-can-place-flowers.py
@@ -0,0 +1,16 @@
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:        
+        count, prev = 0, 0
+        for cur in flowerbed:
+            if cur == 1:
+                if prev == 1:
+                    count -= 1
+                prev = 1
+            else:
+                if prev == 1:
+                    prev = 0 
+                else:
+                    count += 1
+                    prev = 1
+        return count >= n
+                        
