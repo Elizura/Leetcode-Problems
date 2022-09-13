ce62aef0fb9a97f1ea50211e80481edf74c6e8c2
Elizura <98797447+Elizura@users.noreply.github.com>
2022-09-13 06:32:41 -0700

    Time: 74 ms (25.67%), Space: 13.8 MB (76.65%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.py
+++ 376-wiggle-subsequence/376-wiggle-subsequence.py
@@ -1,24 +1,32 @@
 class Solution:
-    def wiggleMaxLength(self, nums: List[int]) -> int:        
-        ans = 1
-        pt = 1
+    def wiggleMaxLength(self, nums: List[int]) -> int:
+        ans = 2
         N = len(nums)
-        if N == 1: return 1
-        for i in range(N):            
-            if i + 1 < N:
-                if nums[i + 1] > nums[i]:
-                    pt = 1
-                elif nums[i + 1] == nums[i]:
-                    continue
-                else:
-                    pt = 0
-            long = 1            
-            for j in range(i + 1, N):                
-                if pt and nums[j] > nums[j - 1]:
-                    long += 1
-                    pt = not pt
-                elif not pt and nums[j] < nums[j - 1]:
-                    long += 1
-                    pt = not pt
-            ans = max(ans, long)
+        dp = [nums[i] - nums[i - 1] for i in range(1, N) if nums[i] - nums[i - 1] != 0]
+        if not dp: return 1
+        for i in range(1, len(dp)):
+            if dp[i - 1] > 0 and dp[i] < 0 or dp[i - 1] < 0 and dp[i] > 0:
+                ans += 1
         return ans
+        # ans = 1
+        # pt = 1
+        # N = len(nums)
+        # if N == 1: return 1
+        # for i in range(N):            
+        #     if i + 1 < N:
+        #         if nums[i + 1] > nums[i]:
+        #             pt = 1
+        #         elif nums[i + 1] == nums[i]:
+        #             continue
+        #         else:
+        #             pt = 0
+        #     long = 1            
+        #     for j in range(i + 1, N):                
+        #         if pt and nums[j] > nums[j - 1]:
+        #             long += 1
+        #             pt = not pt
+        #         elif not pt and nums[j] < nums[j - 1]:
+        #             long += 1
+        #             pt = not pt
+        #     ans = max(ans, long)
+        # return ans
