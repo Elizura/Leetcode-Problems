9875c18780b208efef00f73b134b66a5529150b6
Elizura <98797447+Elizura@users.noreply.github.com>
2022-09-14 05:00:20 -0700

    Time: 285 ms (84.23%), Space: 41.9 MB (5.70%) - LeetHub

--- 494-target-sum/494-target-sum.py
+++ 494-target-sum/494-target-sum.py
@@ -2,17 +2,11 @@ class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:        
         
         @cache
-        def dfs(idx, amt): 
-            # nonlocal ans
+        def dfs(idx, amt):             
             if idx == len(nums):
-                return amt == target
-            # if (idx, ) in dp: return dp[(idx, amt)]
-            # if idx >= len(num+s): return
-            return dfs(idx + 1, amt + nums[idx]) + dfs(idx + 1, amt - nums[idx])
-        # dp = {}
-        ans = 0
-        return dfs(0, 0)
-        return ans
+                return amt == target            
+            return dfs(idx + 1, amt + nums[idx]) + dfs(idx + 1, amt - nums[idx])                
+        return dfs(0, 0)        
     
     
     # class Solution:
