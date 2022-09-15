d3677e13e4cc2ca254031915874d7f905605de81
Elizura <98797447+Elizura@users.noreply.github.com>
2022-09-15 03:36:14 -0700

    Time: 5096 ms (24.93%), Space: 28.1 MB (97.72%) - LeetHub

---
+++ 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minimumTime(self, time: List[int], totalTrips: int) -> int:        
+        def cantrip (timee):
+            trips = 0
+            for i in time:
+                if i <= timee:
+                    trips += timee//i
+            return trips >= totalTrips
+        l, r = 1, min(time)*totalTrips
+        while r >= l:
+            m = (l + r) // 2
+            if cantrip(m):
+                r = m - 1
+            else:
+                l = m + 1
+        return l
