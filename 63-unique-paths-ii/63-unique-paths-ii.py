09d2b85c9b823b978ccf22cae3e51d1b5c2631c5
Elizura <98797447+Elizura@users.noreply.github.com>
2022-09-16 09:53:22 -0700

    Time: 89 ms (18.25%), Space: 14.3 MB (9.60%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.py
+++ 63-unique-paths-ii/63-unique-paths-ii.py
@@ -1,10 +1,13 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-        @cache
+        # @cache
         def dfs(row, col):
+            if (row, col) in memo: return memo[row, col]
             if 0 > row or row >= len(obstacleGrid) or 0 > col or col >= len(obstacleGrid[0]): return 0
             if row == len(obstacleGrid) - 1 and col == len(obstacleGrid[0]) - 1: return 1
             if obstacleGrid[row][col] == 1: return 0
-            return dfs(row + 1, col) + dfs(row, col + 1)
+            memo[row, col] = dfs(row + 1, col) + dfs(row, col + 1)
+            return memo[row, col]
         if obstacleGrid[-1][-1] == 1: return 0
+        memo = {}
         return dfs(0, 0)
