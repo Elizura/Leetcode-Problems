f7dbc4a2a520443ad6cfc04a382d75b27f1472fa
Elizura <98797447+Elizura@users.noreply.github.com>
2022-09-17 23:26:17 -0700

    Time: 239 ms (19.10%), Space: 14.4 MB (14.35%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.py
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.py
@@ -1,6 +1,8 @@
 class Solution:
     def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
-        count = [collections.Counter(mat[i]) for i in range(len(mat))]
-        temp = [(count[i][1], i) for i in range(len(count))]
-        temp.sort()        
-        return [temp[i][-1] for i in range(k)]
+        count = [(-collections.Counter(mat[i])[1], -i) for i in range(len(mat))]           
+        heapq.heapify(count)        
+        while len(count) > k:            
+            heapq.heappop(count)
+        count.sort(reverse = True)
+        return [-count[i][-1] for i in range(len(count))]
