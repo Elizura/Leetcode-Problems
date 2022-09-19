5b7297462934f911be0075827e289971d7b2bc9b
Elizura <98797447+Elizura@users.noreply.github.com>
2022-09-19 01:38:27 -0700

    Time: 70 ms (34.57%), Space: 14.3 MB (15.34%) - LeetHub

--- 486-predict-the-winner/486-predict-the-winner.py
+++ 486-predict-the-winner/486-predict-the-winner.py
@@ -1,24 +1,25 @@
 class Solution:
     def PredictTheWinner(self, nums: List[int]) -> bool:
+        # N = len(nums)
+        # @cache
+        # def dfs(l, r, turn):
+        #     if l == r: return turn * nums[r]                        
+        #     a = turn * nums[l] + dfs(l + 1, r, -turn)
+        #     b = turn * nums[r] + dfs(l, r - 1, -turn)
+        #     return turn * max(turn * a, turn * b)
+        # return dfs(0, N - 1, 1) >= 0
+        
         N = len(nums)
         @cache
-        def dfs(l, r, turn):
-            if l == r: return turn * nums[r]                        
-            a = turn * nums[l] + dfs(l + 1, r, -turn)
-            b = turn * nums[r] + dfs(l, r - 1, -turn)
-            return turn * max(turn * a, turn * b)
-        return dfs(0, N - 1, 1) >= 0
-        
-        # N = len(nums)
-        # def dfs(l, r):
-        #     N = len(nums)
-        #     if l == r: return nums[l]            
-        #     turn = N - (r - l + 1)
-        #     if not turn % 2:                
-        #         return max(nums[l] + dfs(l + 1, r), nums[r] + dfs(l, r - 1))
-        #     else:                 
-        #         return min(-nums[l] + dfs(l + 1, r), -nums[r] + dfs(l, r - 1))                
-        # return dfs(0, N - 1) >= 0
+        def dfs(l, r):
+            N = len(nums)
+            if l == r: return nums[l]            
+            turn = N - (r - l + 1)
+            if not turn % 2:                
+                return max(nums[l] + dfs(l + 1, r), nums[r] + dfs(l, r - 1))
+            else:                 
+                return min(-nums[l] + dfs(l + 1, r), -nums[r] + dfs(l, r - 1))                
+        return dfs(0, N - 1) >= 0
         
 
         
