e9a7a50e2201b321347f56abc0afece466b59f91
Elizura <98797447+Elizura@users.noreply.github.com>
2022-09-19 01:40:34 -0700

    Time: 41 ms (87.52%), Space: 14.1 MB (31.05%) - LeetHub

--- 486-predict-the-winner/486-predict-the-winner.py
+++ 486-predict-the-winner/486-predict-the-winner.py
@@ -10,15 +10,19 @@ class Solution:
         # return dfs(0, N - 1, 1) >= 0
         
         N = len(nums)
-        @cache
+        # @cache
         def dfs(l, r):
+            if (l, r) in memo: return memo[l, r]
             N = len(nums)
             if l == r: return nums[l]            
             turn = N - (r - l + 1)
-            if not turn % 2:                
-                return max(nums[l] + dfs(l + 1, r), nums[r] + dfs(l, r - 1))
-            else:                 
-                return min(-nums[l] + dfs(l + 1, r), -nums[r] + dfs(l, r - 1))                
+            if not turn % 2:
+                memo[l, r] = max(nums[l] + dfs(l + 1, r), nums[r] + dfs(l, r - 1))
+                return memo[l, r]
+            else:   
+                memo[l, r] = min(-nums[l] + dfs(l + 1, r), -nums[r] + dfs(l, r - 1))   
+                return memo[l, r]
+        memo = {}
         return dfs(0, N - 1) >= 0
         
 
