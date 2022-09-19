d57d427c76190997a851b7af70ccdfbf23d7d884
Elizura <98797447+Elizura@users.noreply.github.com>
2022-09-19 02:13:03 -0700

    Time: 38 ms (92.61%), Space: 13.9 MB (85.86%) - LeetHub

--- 486-predict-the-winner/486-predict-the-winner.py
+++ 486-predict-the-winner/486-predict-the-winner.py
@@ -1,5 +1,15 @@
 class Solution:
     def PredictTheWinner(self, nums: List[int]) -> bool:
+        N = len(nums)
+        dp = [[0 for i in range(N)] for i in range(N)]
+        for start in range(N - 1, -1, -1):
+            for end in range(start, N):
+                if start == end:
+                    dp[start][end] = nums[start]
+                else:
+                    dp[start][end] = max(nums[start] - dp[start + 1][end], nums[end] - dp[start][end - 1])
+        return dp[0][-1] >= 0
+        
         # N = len(nums)
         # @cache
         # def dfs(l, r, turn):
@@ -9,21 +19,23 @@ class Solution:
         #     return turn * max(turn * a, turn * b)
         # return dfs(0, N - 1, 1) >= 0
         
-        N = len(nums)
-        # @cache
-        def dfs(l, r):
-            if (l, r) in memo: return memo[l, r]
-            N = len(nums)
-            if l == r: return nums[l]            
-            turn = N - (r - l + 1)
-            if not turn % 2:
-                memo[l, r] = max(nums[l] + dfs(l + 1, r), nums[r] + dfs(l, r - 1))
-                return memo[l, r]
-            else:   
-                memo[l, r] = min(-nums[l] + dfs(l + 1, r), -nums[r] + dfs(l, r - 1))   
-                return memo[l, r]
-        memo = {}
-        return dfs(0, N - 1) >= 0
+        # N = len(nums)
+        # # @cache
+        # def dfs(l, r):
+        #     if (l, r) in memo: return memo[l, r]
+        #     N = len(nums)
+        #     if l == r: return nums[l]            
+        #     turn = N - (r - l + 1)
+        #     if not turn % 2:
+        #         memo[l, r] = max(nums[l] + dfs(l + 1, r), nums[r] + dfs(l, r - 1))
+        #         return memo[l, r]
+        #     else:   
+        #         memo[l, r] = min(-nums[l] + dfs(l + 1, r), -nums[r] + dfs(l, r - 1))   
+        #         return memo[l, r]
+        # memo = {}
+        # return dfs(0, N - 1) >= 0
+        
+        
         
 
         
