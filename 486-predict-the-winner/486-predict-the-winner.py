a17904843222f125ced2e0ba2b38ef1f2a62ed1b
Elizura <98797447+Elizura@users.noreply.github.com>
2022-09-19 02:32:42 -0700

    Time: 40 ms (88.82%), Space: 13.8 MB (85.86%) - LeetHub

--- 486-predict-the-winner/486-predict-the-winner.py
+++ 486-predict-the-winner/486-predict-the-winner.py
@@ -1,14 +1,30 @@
 class Solution:
     def PredictTheWinner(self, nums: List[int]) -> bool:
+        
         N = len(nums)
-        dp = [[0 for i in range(N)] for i in range(N)]
-        for start in range(N - 1, -1, -1):
+        dp = [0 for i in range(N)]
+        dp[-1] = nums[-1]
+        for start in range(N - 2, -1, -1):
+            temp = [0 for i in range(N)]
             for end in range(start, N):
                 if start == end:
-                    dp[start][end] = nums[start]
+                    temp[end] = nums[start]
                 else:
-                    dp[start][end] = max(nums[start] - dp[start + 1][end], nums[end] - dp[start][end - 1])
-        return dp[0][-1] >= 0
+                    temp[end] = max(-dp[end] + nums[start], -temp[end - 1] + nums[end])
+            dp = temp
+        print(dp)
+        return dp[-1] >= 0
+    
+    
+        # N = len(nums)
+        # dp = [[0 for i in range(N)] for i in range(N)]
+        # for start in range(N - 1, -1, -1):
+        #     for end in range(start, N):
+        #         if start == end:
+        #             dp[start][end] = nums[start]
+        #         else:
+        #             dp[start][end] = max(nums[start] - dp[start + 1][end], nums[end] - dp[start][end - 1])
+        # return dp[0][-1] >= 0
         
         # N = len(nums)
         # @cache
