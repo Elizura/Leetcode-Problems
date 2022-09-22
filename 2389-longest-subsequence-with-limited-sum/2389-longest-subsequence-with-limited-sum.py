51e53998c9b4207b1dcebbd298286c0867bd01ef
Elizura <98797447+Elizura@users.noreply.github.com>
2022-09-22 03:41:35 -0700

    Time: 273 ms (53.95%), Space: 14.3 MB (8.52%) - LeetHub

---
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.py
@@ -0,0 +1,25 @@
+class Solution:
+    def answerQueries(self, nums: List[int], queries: List[int]) -> List[int]:
+        nums.sort()
+        N = len(queries)
+        answer = [0]*N
+        pref = [nums[0]]
+        for i in range(1, len(nums)):
+            pref.append(pref[-1] + nums[i])
+            
+        def helper(idx): #binary search helper function
+            # return bisect_right(pref, queries[idx])
+            N = len(pref)
+            l, r = 0, N - 1
+            while r >= l:
+                m = (l + r)//2
+                if pref[m] > queries[idx]:
+                    r = m - 1
+                else: # if it is less than or equal to
+                    l = m + 1
+            return l
+            
+        for j in range(N):
+            answer[j] = helper(j)
+        return answer
+        
