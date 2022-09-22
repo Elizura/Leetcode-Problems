7809fc5a664dfbcf6bb0f156344cecfcc35363e2
Elizura <98797447+Elizura@users.noreply.github.com>
2022-09-22 03:41:52 -0700

    Time: 239 ms (58.02%), Space: 14.1 MB (80.10%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.py
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.py
@@ -8,16 +8,16 @@ class Solution:
             pref.append(pref[-1] + nums[i])
             
         def helper(idx): #binary search helper function
-            # return bisect_right(pref, queries[idx])
-            N = len(pref)
-            l, r = 0, N - 1
-            while r >= l:
-                m = (l + r)//2
-                if pref[m] > queries[idx]:
-                    r = m - 1
-                else: # if it is less than or equal to
-                    l = m + 1
-            return l
+            return bisect_right(pref, queries[idx])
+            # N = len(pref)
+            # l, r = 0, N - 1
+            # while r >= l:
+            #     m = (l + r)//2
+            #     if pref[m] > queries[idx]:
+            #         r = m - 1
+            #     else: # if it is less than or equal to
+            #         l = m + 1
+            # return l
             
         for j in range(N):
             answer[j] = helper(j)
