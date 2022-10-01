1ada585f1590b071bd4507625bc4b8297324a7e4
Elizura <98797447+Elizura@users.noreply.github.com>
2022-10-01 13:39:29 -0700

    Time: 246 ms (84.91%), Space: 15.7 MB (41.99%) - LeetHub

--- 503-next-greater-element-ii/503-next-greater-element-ii.py
+++ 503-next-greater-element-ii/503-next-greater-element-ii.py
@@ -1,12 +1,22 @@
 class Solution:
-    def nextGreaterElements(self, nums: List[int]) -> List[int]:        
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:  
+        stack = [] #store the damn index
         N = len(nums)
-        temp = nums + nums
         ans = [-1] * N
-        stack = [] #stores an index
-        for idx, num in enumerate(temp):
-            while stack and num > nums[stack[-1]]:
-                a = stack.pop()
-                ans[a % N] = num
-            stack.append(idx % N)
+        for i in range(2 * N - 1, -1, -1):
+            while stack and nums[stack[-1]] <= nums[i % N]:
+                stack.pop()
+            if stack:
+                ans[i % N] = nums[stack[-1]]
+            stack.append(i % N)
         return ans
+        # N = len(nums)
+        # temp = nums + nums
+        # ans = [-1] * N
+        # stack = [] #stores an index
+        # for idx, num in enumerate(temp):
+        #     while stack and num > nums[stack[-1]]:
+        #         a = stack.pop()
+        #         ans[a % N] = num
+        #     stack.append(idx % N)
+        # return ans
