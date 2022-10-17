7e531f88eefeeb3579cb3c09e3d69f5c10f6703e
Elizura <98797447+Elizura@users.noreply.github.com>
2022-10-17 08:42:49 -0700

    Time: 1066 ms (74.08%), Space: 28 MB (35.80%) - LeetHub

--- 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.py
+++ 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.py
@@ -1,16 +1,29 @@
 class Solution:
-    def getWinner(self, arr: List[int], k: int) -> int:        
-        dic = defaultdict(int)
-        left, right = 0, 1
+    def getWinner(self, arr: List[int], k: int) -> int:   
+        left, wins = 0, 0
         N = len(arr)
         if k >= N: return max(arr)
-        while right < N:
+        if k == 1: return max(arr[0], arr[1])
+        for right in range(1, N):
             if arr[left] > arr[right]:
-                dic[arr[left]] += 1
-            else:
-                dic[arr[right]] += 1
+                wins += 1
+                if wins == k: return arr[left]
+            elif arr[right] > arr[left]:
                 left = right
-            if dic[arr[left]] == k:
-                return arr[left]
-            right += 1
-            if right >= N: right %= N
+                wins = 1    
+        return max(arr)
+                
+        # dic = defaultdict(int)
+        # left, right = 0, 1
+        # N = len(arr)
+        # if k >= N: return max(arr)
+        # while right < N:
+        #     if arr[left] > arr[right]:
+        #         dic[arr[left]] += 1
+        #     else:
+        #         dic[arr[right]] += 1
+        #         left = right
+        #     if dic[arr[left]] == k:
+        #         return arr[left]
+        #     right += 1
+        #     if right >= N: right %= N
