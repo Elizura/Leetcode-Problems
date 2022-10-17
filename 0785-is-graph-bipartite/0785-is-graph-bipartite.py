2fa6bf1c5867e3c8333dd906e8024326268a5d54
Elizura <98797447+Elizura@users.noreply.github.com>
2022-10-17 07:28:54 -0700

    Time: 389 ms (46.65%), Space: 14.7 MB (27.89%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.py
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.py
@@ -1,22 +1,37 @@
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
-        def bfs(ver):
-            color = [-1] * len(graph)
-            q = deque([ver])
-            color[ver] = 1
-            while q:
-                a = q.popleft()
-                seen.add(a)
-                for node in graph[a]:
-                    if color[node] == -1:
-                        color[node] = 1 - color[a]
-                        q.append(node)
-                    else:
-                        if color[node] == color[a]: return False
+        def dfs(node):
+            for neighbour in graph[node]:
+                if neighbour in color:
+                    if color[neighbour] == color[node]: return False
+                else:
+                    color[neighbour] = 1 - color[node]
+                    if not dfs(neighbour): return False
             return True
-        
-        seen = set()
+        color = {}
         for i in range(len(graph)):
-            if i not in seen:
-                if not bfs(i): return False
+            if i not in color:
+                color[i] = 0
+                if not dfs(i): return False
         return True
+            
+#         def bfs(ver):
+#             color = [-1] * len(graph)
+#             q = deque([ver])
+#             color[ver] = 1
+#             while q:
+#                 a = q.popleft()
+#                 seen.add(a)
+#                 for node in graph[a]:
+#                     if color[node] == -1:
+#                         color[node] = 1 - color[a]
+#                         q.append(node)
+#                     else:
+#                         if color[node] == color[a]: return False
+#             return True
+        
+#         seen = set()
+#         for i in range(len(graph)):
+#             if i not in seen:
+#                 if not bfs(i): return False
+#         return True
