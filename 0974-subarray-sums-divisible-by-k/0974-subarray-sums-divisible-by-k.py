e53ebe211e4fbd870e099026f29129719d68dc24
Elizura <98797447+Elizura@users.noreply.github.com>
2022-10-17 12:22:25 -0700

    Time: 750 ms (29.73%), Space: 18.9 MB (23.28%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.py
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.py
@@ -6,21 +6,20 @@ class Solution:
         for num in nums:
             pref += num
             t = pref % k
-            if t in dic:
-                a = dic[t]
-                ans += a
-                dic[t] = a + 1
+            if t in dic:                
+                ans += dic[t]
+                dic[t] += 1
             else:
                 dic[t] = 1
         return ans    
     
-# class Solution:
-#     def subarraysDivByK(self, A, K):
-#         csum, ans = 0, 0
-#         D = [0] * K
-#         D[0] = 1
-#         for i in A:
-#             csum = (csum + i) % K
-#             ans += D[csum]
-#             D[csum] += 1
-#         return ans
+class Solution:
+    def subarraysDivByK(self, A, K):
+        csum, ans = 0, 0
+        D = [0] * K
+        D[0] = 1
+        for i in A:
+            csum = (csum + i) % K
+            ans += D[csum]
+            D[csum] += 1
+        return ans
