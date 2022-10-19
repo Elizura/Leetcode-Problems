c4bb8eb52754a791c972c3e1dd0383b99675d8a6
Elizura <98797447+Elizura@users.noreply.github.com>
2022-10-19 06:12:08 -0700

    Time: 4032 ms (18.60%), Space: 27.7 MB (93.24%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.py
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.py
@@ -1,19 +1,13 @@
 class Solution:
     def maxFrequency(self, nums: List[int], k: int) -> int:
         nums.sort()
-        largest_freq=0
-        l=r=0 #right and left pointers set to 0
-        total=0 #basically used for the formula but nothing else
-        for i in range(len(nums)):
-            total+=nums[r]
-            
-            while nums[r]*(r-l+1)>k+total:
-                #subtract the number from the total
-                #shrink the size by moving the left pointer forwards
-                total-=nums[l]
-                l+=1
-            largest_freq=max(largest_freq,r-l+1) #updating the largest frequency
-            r+=1
-        return largest_freq
-                
-        
+        pref = 0
+        left = 0
+        max_freq = 0
+        for right, num in enumerate(nums):            
+            pref += num
+            while pref + k < num * (right - left + 1):                
+                pref -= nums[left]
+                left += 1
+            max_freq = max(max_freq, right - left + 1)
+        return max_freq
