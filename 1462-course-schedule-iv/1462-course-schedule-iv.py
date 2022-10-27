681ba1cade5fe2bd5b582a10cafa4a8a2064b63f
Elizura <98797447+Elizura@users.noreply.github.com>
2022-10-27 00:54:11 -0700

    Time: 5640 ms (9.74%), Space: 17.6 MB (82.92%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.py
+++ 1462-course-schedule-iv/1462-course-schedule-iv.py
@@ -1,35 +1,47 @@
 class Solution:
     def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
-        graph = defaultdict(list)
-        visited = [False] * numCourses
-        indegree = [0] * numCourses
-        for a, b in prerequisites:
-            graph[a].append(b)
-            indegree[b] += 1        
-        ancestors = [set() for _ in range(numCourses)]
-        q = deque()
-        # dic = {}
-        # level = 0
-        for i in range(numCourses):
-            if indegree[i] == 0:
-                q.append(i)                        
-        while q:
-            for i in range(len(q)):
-                a = q.popleft()                
-                # dic[a] = level
-                for n in graph[a]:
-                    if not visited[n]:
-                        ancestors[n].add(a)
-                        ancestors[n].update(ancestors[a])
-                        indegree[n] -= 1
-                        if indegree[n] == 0:
-                            q.append(n)
-                            visited[n] = True
-            # level += 1             
-        N = len(queries)
-        ans = [False] * N
-        for i in range(N):            
-            a, b = queries[i]
-            if a in ancestors[b]:
-                ans[i] = True
-        return ans
+        n = numCourses
+        connected = [[False] * n for i in range(n)]
+
+        for i, j in prerequisites:
+            connected[i][j] = True
+
+        for k in range(n):
+            for i in range(n):
+                for j in range(n):
+                    connected[i][j] = connected[i][j] or (connected[i][k] and connected[k][j])
+
+        return [connected[i][j] for i, j in queries]
+        # graph = defaultdict(list)
+        # visited = [False] * numCourses
+        # indegree = [0] * numCourses
+        # for a, b in prerequisites:
+        #     graph[a].append(b)
+        #     indegree[b] += 1        
+        # ancestors = [set() for _ in range(numCourses)]
+        # q = deque()
+        # # dic = {}
+        # # level = 0
+        # for i in range(numCourses):
+        #     if indegree[i] == 0:
+        #         q.append(i)                        
+        # while q:
+        #     for i in range(len(q)):
+        #         a = q.popleft()                
+        #         # dic[a] = level
+        #         for n in graph[a]:
+        #             if not visited[n]:
+        #                 ancestors[n].add(a)
+        #                 ancestors[n].update(ancestors[a])
+        #                 indegree[n] -= 1
+        #                 if indegree[n] == 0:
+        #                     q.append(n)
+        #                     visited[n] = True
+        #     # level += 1             
+        # N = len(queries)
+        # ans = [False] * N
+        # for i in range(N):            
+        #     a, b = queries[i]
+        #     if a in ancestors[b]:
+        #         ans[i] = True
+        # return ans
