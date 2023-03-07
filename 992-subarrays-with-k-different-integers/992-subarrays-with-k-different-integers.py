4bef96f9ef5e25cd4be1502efe6296cc24e4a66d
Elisur Freyesus <98797447+Elizura@users.noreply.github.com>
2023-03-06 20:04:04 -0800

    Time: 486 ms (86.92%), Space: 16.7 MB (56.12%) - LeetHub

--- 992-subarrays-with-k-different-integers/992-subarrays-with-k-different-integers.py
+++ 992-subarrays-with-k-different-integers/992-subarrays-with-k-different-integers.py
@@ -1,21 +1,19 @@
 class Solution:
-    def atMostKDistinct(self, nums, k):
-        hashmap = {}
-        subarrays = 0
-        start, end = 0, 0
-        while end < len(nums):
-            if nums[end] in hashmap:
-                hashmap[nums[end]] += 1
-            else:
-                hashmap[nums[end]] = 1
-            while len(hashmap) > k:
-                hashmap[nums[start]] -= 1
-                if hashmap[nums[start]] == 0:
-                    del hashmap[nums[start]]
-                start += 1
-            subarrays += end-start+1
-            end += 1
-        return subarrays
-    
     def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
-        return self.atMostKDistinct(nums, k) - self.atMostKDistinct(nums, k-1)
+        def helper(nums, k):            
+            left = 0
+            N = len(nums)            
+            subarrayCnts = 0
+            numsCnt = {}
+            for right in range(N):
+                numsCnt[nums[right]] = numsCnt.get(nums[right], 0) + 1
+                while len(numsCnt) > k:
+                    numsCnt[nums[left]] -= 1
+                    if not numsCnt[nums[left]]:
+                        del numsCnt[nums[left]]
+                    left += 1   
+                subarrayCnts += right - left + 1                     
+            return subarrayCnts
+        
+        return helper(nums, k) - helper(nums, k - 1)
+        
