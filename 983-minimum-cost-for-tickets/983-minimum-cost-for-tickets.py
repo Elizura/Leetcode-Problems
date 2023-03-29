8f818894ba55fe5ab025b99b0d7d215d7afacaaf
Elisur Freyesus <98797447+Elizura@users.noreply.github.com>
2023-03-29 05:24:09 +0200

    Time: 35 ms (98.50%), Space: 13.9 MB (83.55%) - LeetHub

--- 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.py
+++ 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.py
@@ -1,9 +1,14 @@
 class Solution:
     def mincostTickets(self, days: List[int], cost: List[int]) -> int:
         days = set(days)    
-        @cache
-        def dfs(day):
-            if day > 365: return 0
-            if day not in days: return dfs(day + 1)
-            return min((dfs(day + 1) + cost[0]), (dfs(day + 7) + cost[1]), (dfs(day + 30) + cost[2]))
-        return dfs(0)
+        # @cache
+        # def dfs(day):
+        #     if day > 365: return 0
+        #     if day not in days: return dfs(day + 1)
+        #     return min((dfs(day + 1) + cost[0]), (dfs(day + 7) + cost[1]), (dfs(day + 30) + cost[2]))
+        # return dfs(0)
+        dp = [0]*400        
+        for i in range(364, -1, -1):
+            if i + 1 not in days: dp[i] = dp[i + 1]; continue
+            dp[i] = min(cost[0] + dp[i + 1], cost[1] + dp[i + 7], cost[2] + dp[i + 30])        
+        return dp[0]
